8e529bf06112ec1e36fea96950fed37c7598558d
Davis King <davis@dlib.net>
2009-06-30 23:23:57 +0000

    Just retabbed this file.
    --HG--
    extra : convert_revision : svn%3Afdd8eb12-d10e-0410-9acb-85c331704f74/trunk%403106

--- dlib/smart_pointers/scoped_ptr.h
+++ dlib/smart_pointers/scoped_ptr.h
@@ -12,74 +12,74 @@ namespace dlib
 {
 
     template<typename T> 
-        class scoped_ptr : noncopyable 
+    class scoped_ptr : noncopyable 
+    {
+        /*!
+            CONVENTION
+                - get() == ptr
+        !*/
+
+    public:
+        typedef T element_type;
+
+        explicit scoped_ptr (
+            T* p = 0
+        ) : ptr(p) { }
+
+        ~scoped_ptr() { if (ptr) delete ptr; }
+
+        void reset (
+            T* p = 0
+        ) 
+        { 
+            if (ptr) 
+                delete ptr; 
+            ptr = p;
+        }
+
+        T& operator*() const
         {
-            /*!
-                CONVENTION
-                    - get() == ptr
-            !*/
+            DLIB_ASSERT(get() != 0,
+                        "\tscoped_ptr::operator*()"
+                        << "\n\tget() can't be null if you are going to dereference it"
+                        << "\n\tthis: " << this
+            );
 
-        public:
-            typedef T element_type;
+            return *ptr;
+        }
 
-            explicit scoped_ptr (
-                T* p = 0
-            ) : ptr(p) { }
+        T* operator->() const
+        {
+            DLIB_ASSERT(get() != 0,
+                        "\tscoped_ptr::operator*()"
+                        << "\n\tget() can't be null"
+                        << "\n\tthis: " << this
+            );
 
-            ~scoped_ptr() { if (ptr) delete ptr; }
+            return ptr;
+        }
 
-            void reset (
-                T* p = 0
-            ) 
-            { 
-                if (ptr) 
-                    delete ptr; 
-                ptr = p;
-            }
+        T* get() const
+        {
+            return ptr;
+        }
 
-            T& operator*() const
-            {
-                DLIB_ASSERT(get() != 0,
-                    "\tscoped_ptr::operator*()"
-                    << "\n\tget() can't be null if you are going to dereference it"
-                    << "\n\tthis: " << this
-                    );
+        operator bool() const
+        {
+            return (ptr != 0);
+        }
 
-                return *ptr;
-            }
+        void swap(
+            scoped_ptr& b
+        )
+        {
+            std::swap(ptr,b.ptr);
+        }
 
-            T* operator->() const
-            {
-                DLIB_ASSERT(get() != 0,
-                    "\tscoped_ptr::operator*()"
-                    << "\n\tget() can't be null"
-                    << "\n\tthis: " << this
-                    );
+    private:
 
-                return ptr;
-            }
-
-            T* get() const
-            {
-                return ptr;
-            }
-
-            operator bool() const
-            {
-                return (ptr != 0);
-            }
-
-            void swap(
-                scoped_ptr& b
-            )
-            {
-                std::swap(ptr,b.ptr);
-            }
-
-        private:
-
-            T* ptr;
-        };
+        T* ptr;
+    };
 
     template <
         typename T
