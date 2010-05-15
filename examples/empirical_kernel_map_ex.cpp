abc868779c4a7e5d52da66031dfb22b25a756059
Davis King <davis@dlib.net>
2010-05-15 16:15:16 +0000

    Updated example to use new ekm.load(lisf) function.
    --HG--
    extra : convert_revision : svn%3Afdd8eb12-d10e-0410-9acb-85c331704f74/trunk%403617

--- examples/empirical_kernel_map_ex.cpp
+++ examples/empirical_kernel_map_ex.cpp
@@ -153,7 +153,7 @@ int main()
     // Now reload the empirical_kernel_map but this time using only our 20 basis samples 
     // selected using the linearly_independent_subset_finder.
     cout << "\n\nBuilding an empirical_kernel_map with " << lisf.dictionary_size() << " basis samples." << endl;
-    ekm.load(kern, lisf.get_dictionary());
+    ekm.load(lisf);
     cout << "Test the empirical_kernel_map when loaded with samples from the lisf object." << endl;
     test_empirical_kernel_map(samples, labels, ekm);
 
