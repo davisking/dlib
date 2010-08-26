b6f6ab72193b7e7b41522b902bbe6b91890a846f
Davis King <davis@dlib.net>
2010-08-26 15:16:54 +0000

    Updated hyperlink
    --HG--
    extra : convert_revision : svn%3Afdd8eb12-d10e-0410-9acb-85c331704f74/trunk%403811

--- examples/bayes_net_gui_ex.cpp
+++ examples/bayes_net_gui_ex.cpp
@@ -629,7 +629,7 @@ on_menu_help_about (
     message_box("About","This application is the GUI front end to the dlib C++ Library's\n"
                 "Bayesian Network inference utilities\n\n"
                 "Version 1.2\n\n"
-                "See http://dclib.sourceforge.net for updates");
+                "See http://dlib.net for updates");
 }
 
 // ----------------------------------------------------------------------------------------
