30e8807f3bead63fb6902b5046cd3aecb72e51f5
Davis King <davis@dlib.net>
2010-12-04 17:00:17 +0000

    Added an any container object.
    --HG--
    extra : convert_revision : svn%3Afdd8eb12-d10e-0410-9acb-85c331704f74/trunk%403942

---
+++ dlib/any.h
@@ -0,0 +1,10 @@
+// Copyright (C) 2010  Davis E. King (davis@dlib.net)
+// License: Boost Software License   See LICENSE.txt for the full license.
+#ifndef DLIB_AnY_
+#define DLIB_AnY_
+
+#include "any/any.h"
+
+#endif // DLIB_AnY_
+
+
