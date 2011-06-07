cf6f55c2fa92c61367b353643969ca4b387a0ad8
Davis King <davis@dlib.net>
2011-06-06 21:32:14 -0400

    Clarified spec again

--- dlib/any/any_abstract.h
+++ dlib/any/any_abstract.h
@@ -159,7 +159,8 @@ namespace dlib
             ensures
                 - swaps *this and item
                 - does not invalidate pointers or references to the object contained 
-                  inside *this or item.
+                  inside *this or item.  Moreover, a pointer or reference to the object in 
+                  *this will now refer to the contents of #item and vice versa.
         !*/
 
     };
