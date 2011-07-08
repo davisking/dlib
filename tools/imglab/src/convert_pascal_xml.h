a0bb4ea8df02f3c2976afb7a7e081556868ccd7f
Davis King <davis@dlib.net>
2011-07-08 15:48:48 -0400

    renamed a file
    --HG--
    rename : tools/imglab/src/convert_pascal_voc.h => tools/imglab/src/convert_pascal_xml.h

---
+++ tools/imglab/src/convert_pascal_xml.h
@@ -0,0 +1,11 @@
+// Copyright (C) 2011  Davis E. King (davis@dlib.net)
+// License: Boost Software License   See LICENSE.txt for the full license.
+#ifndef DLIB_IMGLAB_CONVERT_PASCAl_XML_H__
+#define DLIB_IMGLAB_CONVERT_PASCAl_XML_H__
+
+#include "common.h"
+
+void convert_pascal_xml(const parser_type& parser);
+
+#endif // DLIB_IMGLAB_CONVERT_PASCAl_XML_H__
+
