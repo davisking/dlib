12e6f8eeec9c0bbe0ee0b0c6741cad15cdb41f91
Davis King <davis@dlib.net>
2011-09-07 23:45:14 -0400

    Added some comments.  Still need to complete the spec.

--- dlib/svm/structural_svm_object_detection_problem_abstract.h
+++ dlib/svm/structural_svm_object_detection_problem_abstract.h
@@ -20,22 +20,53 @@ namespace dlib
     class structural_svm_object_detection_problem : public structural_svm_problem_threaded<matrix<double,0,1> >,
                                                     noncopyable
     {
+        /*!
+            REQUIREMENTS ON image_scanner_type
+                image_scanner_type must be an instance of the scan_image_pyramid
+                templated defined in dlib/image_processing/scan_image_pyramid_abstract.h
+                or an object with a compatible interface.
+
+            REQUIREMENTS ON overlap_tester_type
+                overlap_tester_type must be a type with an interface compatible
+                with test_box_overlap.
+
+            REQUIREMENTS ON image_array_type
+                image_array_type must be a dlib/array/array_kernel_abstract.h object
+                which contains object types which be accepted by image_scanner_type::load().
+
+            WHAT THIS OBJECT REPRESENTS
+                This object is a tool for learning the parameter vector needed to use
+                a scan_image_pyramid object.  
+        !*/
     public:
 
         structural_svm_object_detection_problem(
             const image_scanner_type& scanner,
             const overlap_tester_type& overlap_tester,
-            const image_array_type& images_,
-            const std::vector<std::vector<rectangle> >& rects_,
+            const image_array_type& images,
+            const std::vector<std::vector<rectangle> >& rects,
             unsigned long num_threads = 2
         );
+        /*!
+            requires
+                - images.size() == rects.size()
+                - scanner.num_detection_templates() > 0
+        !*/
 
         void set_overlap_eps (
             double eps
         );
+        /*!
+            requires
+                - eps > 0
+            ensures
+                - #get_overlap_eps() == eps
+        !*/
 
         double get_overlap_eps (
         ) const;
+        /*!
+        !*/
 
     };
 
