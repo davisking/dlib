37bc7b23bda5ea209fb0e36986d7a4ee3bf137b2
Davis King <davis@dlib.net>
2012-04-28 16:04:50 -0400

    Fixed typo in inclusion guard.

--- dlib/optimization.h
+++ dlib/optimization.h
@@ -1,7 +1,7 @@
 // Copyright (C) 2008  Davis E. King (davis@dlib.net)
 // License: Boost Software License   See LICENSE.txt for the full license.
 #ifndef DLIB_OPTIMIZATIOn_HEADER
-#define DLIB_SVM_HEADER
+#define DLIB_OPTIMIZATIOn_HEADER
 
 #include "optimization/optimization.h"
 #include "optimization/optimization_bobyqa.h"
