8a8432111041cd34bd9c7222743b0e42d37a5184
Davis King <davis@dlib.net>
2012-08-07 20:55:43 -0400

    Removed cruft from spec file.

--- dlib/graph_cuts/min_cut_abstract.h
+++ dlib/graph_cuts/min_cut_abstract.h
@@ -22,9 +22,6 @@ namespace dlib
 
 // ----------------------------------------------------------------------------------------
 
-    template <
-        typename directed_graph_type
-        >
     class flow_graph 
     {
         /*!
