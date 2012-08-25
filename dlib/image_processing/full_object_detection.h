a48cde58a981b4166d804e169326ee0f85405558
Davis King <davis@dlib.net>
2012-08-24 20:22:27 -0400

    fleshed the full_object_detection out a little more.

--- dlib/image_processing/full_object_detection.h
+++ dlib/image_processing/full_object_detection.h
@@ -24,6 +24,8 @@ namespace dlib
             const std::vector<point>& movable_parts_
         ) : rect(rect_), movable_parts(movable_parts_) {}
 
+        full_object_detection(){}
+
         explicit full_object_detection(
             const rectangle& rect_
         ) : rect(rect_) {}
@@ -31,6 +33,21 @@ namespace dlib
         rectangle rect;
         std::vector<point> movable_parts;  // it should always be the case that rect.contains(movable_parts[i]) == true
     };
+
+// ----------------------------------------------------------------------------------------
+
+    inline bool all_parts_in_rect (
+        const full_object_detection& obj
+    )
+    {
+        for (unsigned long i = 0; i < obj.movable_parts.size(); ++i)
+        {
+            if (obj.rect.contains(obj.movable_parts[i]) == false &&
+                obj.movable_parts[i] != MOVABLE_PART_NOT_PRESENT)
+                return false;
+        }
+        return true;
+    }
 
 // ----------------------------------------------------------------------------------------
 
