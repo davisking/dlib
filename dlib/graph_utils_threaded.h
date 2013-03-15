0cb100d3576d98a8816b6646ebae2bd001f050b4
Davis King <davis@dlib.net>
2013-03-14 21:33:32 -0400

    Made graph_utils_threaded.h #include graph_utils.h

--- dlib/graph_utils_threaded.h
+++ dlib/graph_utils_threaded.h
@@ -3,6 +3,7 @@
 #ifndef DLIB_GRAPH_UTILs_THREADED_H_
 #define DLIB_GRAPH_UTILs_THREADED_H_ 
 
+#include "graph_utils.h"
 #include "graph_utils/find_k_nearest_neighbors_lsh.h"
 
 #endif // DLIB_GRAPH_UTILs_THREADED_H_ 
