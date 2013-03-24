b95b2e14f95cac1905ba90a82f87a9fd46a6a7df
Davis King <davis@dlib.net>
2013-03-24 16:29:39 -0400

    Added a version of setup_hashed_features() for scan_image_boxes objects.

--- dlib/image_processing/setup_hashed_features.h
+++ dlib/image_processing/setup_hashed_features.h
@@ -25,6 +25,10 @@ namespace dlib
     };
 
 // ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+//                                 stuff for scan_image_pyramid
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
 
     template <
         typename image_array,
@@ -75,6 +79,79 @@ namespace dlib
         >
     void setup_hashed_features (
         scan_image_pyramid<pyramid, hashed_feature_image<feature_extractor, projection_hash> >& scanner,
+        const image_array& images,
+        int bits,
+        unsigned long num_samples = 200000
+    )
+    {
+        // make sure requires clause is not broken
+        DLIB_ASSERT(0 < bits && bits <= 32 &&
+                    num_samples > 1 && 
+                    images.size() > 0,
+            "\t void setup_hashed_features()"
+            << "\n\t Invalid inputs were given to this function. "
+            << "\n\t bits:          " << bits 
+            << "\n\t num_samples:   " << num_samples 
+            << "\n\t images.size(): " << images.size() 
+            );
+
+        feature_extractor fe;
+        setup_hashed_features(scanner, images, fe, bits, num_samples);
+    }
+
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+//                                 stuff for scan_image_boxes
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+
+    template <
+        typename image_array,
+        typename feature_extractor
+        >
+    void setup_hashed_features (
+        scan_image_boxes<hashed_feature_image<feature_extractor, projection_hash> >& scanner,
+        const image_array& images,
+        const feature_extractor& fe,
+        int bits,
+        unsigned long num_samples = 200000
+    )
+    {
+        // make sure requires clause is not broken
+        DLIB_ASSERT(0 < bits && bits <= 32 &&
+                    num_samples > 1 && 
+                    images.size() > 0,
+            "\t void setup_hashed_features()"
+            << "\n\t Invalid inputs were given to this function. "
+            << "\n\t bits:          " << bits 
+            << "\n\t num_samples:   " << num_samples 
+            << "\n\t images.size(): " << images.size() 
+            );
+
+        pyramid_disable pyr;
+
+        const random_subset_selector<typename feature_extractor::descriptor_type>& samps = 
+            randomly_sample_image_features(images, pyr, fe, num_samples);
+
+        if (samps.size() <= 1)
+            throw dlib::image_hash_construction_failure("Images too small, not able to gather enough samples to make hash");
+
+        projection_hash phash = create_random_projection_hash(samps, bits);
+
+        hashed_feature_image<feature_extractor, projection_hash> hfe;
+        hfe.set_hash(phash);
+        hfe.copy_configuration(fe);
+        scanner.copy_configuration(hfe);
+    }
+
+// ----------------------------------------------------------------------------------------
+
+    template <
+        typename image_array,
+        typename feature_extractor
+        >
+    void setup_hashed_features (
+        scan_image_boxes<hashed_feature_image<feature_extractor, projection_hash> >& scanner,
         const image_array& images,
         int bits,
         unsigned long num_samples = 200000
