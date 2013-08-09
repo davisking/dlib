b68e5a37fbf770d911da53e948e76eb9fdf10bcc
Davis King <davis@dlib.net>
2013-08-09 11:23:05 -0400

    Added functions for easily converting between python list objects and std::vector.

--- tools/python/src/boost_python_utils.h
+++ tools/python/src/boost_python_utils.h
@@ -20,5 +20,42 @@ inline bool hasattr(
      return PyObject_HasAttrString(obj.ptr(), attr_name.c_str());
 }
 
+// ----------------------------------------------------------------------------------------
+
+template <typename T>
+std::vector<T> python_list_to_vector (
+    const boost::python::object& obj
+)
+/*!
+    ensures
+        - converts a python object into a std::vector<T> and returns it.
+!*/
+{
+    std::vector<T> vect(len(obj));
+    for (unsigned long i = 0; i < vect.size(); ++i)
+    {
+        vect[i] = boost::python::extract<T>(obj[i]);
+    }
+    return vect;
+}
+
+template <typename T>
+boost::python::list vector_to_python_list (
+    const std::vector<T>& vect
+)
+/*!
+    ensures
+        - converts a std::vector<T> into a python list object.
+!*/
+{
+    boost::python::list obj;
+    for (unsigned long i = 0; i < vect.size(); ++i)
+        obj.append(vect[i]);
+    return obj;
+}
+
+// ----------------------------------------------------------------------------------------
+
+
 #endif // DLIB_BOOST_PYTHON_UtILS_H__
 
