f61432c6262be9b9a942a39d8ce2857730f03dac
Davis King <davis@dlib.net>
2014-09-05 18:26:37 -0400

    Changed noncopyable.h to avoid a name clash with boost 1.56

--- dlib/noncopyable.h
+++ dlib/noncopyable.h
@@ -7,40 +7,25 @@
 #ifndef DLIB_BOOST_NONCOPYABLE_HPP_INCLUDED
 #define DLIB_BOOST_NONCOPYABLE_HPP_INCLUDED
 
-#ifndef BOOST_NONCOPYABLE_HPP_INCLUDED
-#define BOOST_NONCOPYABLE_HPP_INCLUDED
-
-namespace boost 
-{
-
-    namespace noncopyable_  // protection from unintended ADL
-    {
-        class noncopyable
-        {
-            /*!
-                This class makes it easier to declare a class as non-copyable. 
-                If you want to make an object that can't be copied just inherit
-                from this object.
-            !*/
-
-        protected:
-            noncopyable() {}
-            ~noncopyable() {}
-        private:  // emphasize the following members are private
-            noncopyable( const noncopyable& );
-            const noncopyable& operator=( const noncopyable& );
-        };
-    }
-
-    typedef noncopyable_::noncopyable noncopyable;
-
-} // namespace boost
-
-#endif // BOOST_NONCOPYABLE_HPP_INCLUDED
 
 namespace dlib
 {
-    using boost::noncopyable;
+    class noncopyable
+    {
+        /*!
+            This class makes it easier to declare a class as non-copyable.
+            If you want to make an object that can't be copied just inherit
+            from this object.
+        !*/
+
+    protected:
+        noncopyable() {}
+        ~noncopyable() {}
+    private:  // emphasize the following members are private
+        noncopyable(const noncopyable&);
+        const noncopyable& operator=(const noncopyable&);
+
+    };
 }
 
 #endif  // DLIB_BOOST_NONCOPYABLE_HPP_INCLUDED
