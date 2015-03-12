154f9e4931a913a0366ece67d7abc39aca1a3273
Jack Culpepper <jack@iqengines.com>
2015-03-11 23:23:19 -0700

    add ability to return detection confidences and weight index to python

--- tools/python/src/simple_object_detector_py.h
+++ tools/python/src/simple_object_detector_py.h
@@ -13,20 +13,47 @@ namespace dlib
 {
     typedef object_detector<scan_fhog_pyramid<pyramid_down<6> > > simple_object_detector;
 
+    inline void split_rect_detections (
+        std::vector<rect_detection>& rect_detections,
+        std::vector<rectangle>& rectangles,
+        std::vector<double>& detection_confidences,
+        std::vector<int>& weight_indices
+    )
+    {
+        rectangles.clear();
+        detection_confidences.clear();
+        weight_indices.clear();
+
+        for (unsigned long i = 0; i < rect_detections.size(); ++i)
+        {
+            rectangles.push_back(rect_detections[i].rect);
+            detection_confidences.push_back(rect_detections[i].detection_confidence);
+            weight_indices.push_back(rect_detections[i].weight_index);
+        }
+    }
+
     inline std::vector<dlib::rectangle> run_detector_with_upscale (
         dlib::simple_object_detector& detector,
         boost::python::object img,
-        const unsigned int upsampling_amount
+        const unsigned int upsampling_amount,
+        std::vector<double>& detection_confidences,
+        std::vector<int>& weight_indices
     )
     {
         pyramid_down<2> pyr;
+
+        std::vector<rectangle> rectangles;
+        std::vector<rect_detection> rect_detections;
 
         if (is_gray_python_image(img))
         {
             array2d<unsigned char> temp;
             if (upsampling_amount == 0)
             {
-                return detector(numpy_gray_image(img));
+                detector(numpy_gray_image(img), rect_detections, 0.0);
+                split_rect_detections(rect_detections, rectangles,
+                                      detection_confidences, weight_indices);
+                return rectangles;
             }
             else
             {
@@ -38,10 +65,14 @@ namespace dlib
                     pyramid_up(temp);
                 }
 
-                std::vector<rectangle> res = detector(temp);
-                for (unsigned long i = 0; i < res.size(); ++i)
-                    res[i] = pyr.rect_down(res[i], upsampling_amount);
-                return res;
+                detector(temp, rect_detections, 0.0);
+                for (unsigned long i = 0; i < rect_detections.size(); ++i)
+                    rect_detections[i].rect = pyr.rect_down(rect_detections[i].rect,
+                                                            upsampling_amount);
+                split_rect_detections(rect_detections, rectangles,
+                                      detection_confidences, weight_indices);
+
+                return rectangles;
             }
         }
         else if (is_rgb_python_image(img))
@@ -49,7 +80,10 @@ namespace dlib
             array2d<rgb_pixel> temp;
             if (upsampling_amount == 0)
             {
-                return detector(numpy_rgb_image(img));
+                detector(numpy_rgb_image(img), rect_detections, 0.0);
+                split_rect_detections(rect_detections, rectangles,
+                                      detection_confidences, weight_indices);
+                return rectangles;
             }
             else
             {
@@ -61,10 +95,14 @@ namespace dlib
                     pyramid_up(temp);
                 }
 
-                std::vector<rectangle> res = detector(temp);
-                for (unsigned long i = 0; i < res.size(); ++i)
-                    res[i] = pyr.rect_down(res[i], upsampling_amount);
-                return res;
+                detector(temp, rect_detections, 0.0);
+                for (unsigned long i = 0; i < rect_detections.size(); ++i)
+                    rect_detections[i].rect = pyr.rect_down(rect_detections[i].rect,
+                                                            upsampling_amount);
+                split_rect_detections(rect_detections, rectangles,
+                                      detection_confidences, weight_indices);
+
+                return rectangles;
             }
         }
         else
@@ -82,11 +120,40 @@ namespace dlib
         simple_object_detector_py(simple_object_detector& _detector, unsigned int _upsampling_amount) :
             detector(_detector), upsampling_amount(_upsampling_amount) {}
 
-        std::vector<dlib::rectangle> run_detector1 (boost::python::object img, const unsigned int upsampling_amount_)
-        { return run_detector_with_upscale(detector, img, upsampling_amount_); }
+        std::vector<dlib::rectangle> run_detector1 (boost::python::object img,
+                                                    const unsigned int upsampling_amount_)
+        {
+            std::vector<double> detection_confidences;
+            std::vector<int> weight_indices;
+
+            return run_detector_with_upscale(detector, img, upsampling_amount_,
+                detection_confidences, weight_indices);
+        }
 
         std::vector<dlib::rectangle> run_detector2 (boost::python::object img)
-        { return run_detector_with_upscale(detector, img, upsampling_amount); }
+        {
+            std::vector<double> detection_confidences;
+            std::vector<int> weight_indices;
+            return run_detector_with_upscale(detector, img, upsampling_amount,
+                detection_confidences, weight_indices);
+        }
+
+
+        boost::python::tuple run_detector3 (boost::python::object img,
+                                            const unsigned int upsampling_amount_)
+        {
+            boost::python::tuple t;
+
+            std::vector<double> detection_confidences;
+            std::vector<int> weight_indices;
+            std::vector<rectangle> rectangles;
+
+            rectangles = run_detector_with_upscale(detector, img, upsampling_amount,
+                                                   detection_confidences, weight_indices);
+
+            return boost::python::make_tuple(rectangles,
+                                             detection_confidences, weight_indices);
+        }
     };
 }
 
