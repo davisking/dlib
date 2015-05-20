c4cf31a746e1e20908aff0f46ed9b2fd94ec25ee
Patrick Snape <patricksnape@gmail.com>
2015-05-20 10:46:01 +0100

    Preparing for correlation tracker - add drectangle
    The correlation tracker deals with the drectangle
    (double rectangle) class which currently isn't wrapped. Therefore,
    I add the drectangle class and refactor rectangles into their
    own file. I also added a load of methods on rectangle that might
    be useful such as intersection/contains/area etc.

--- tools/python/src/dlib.cpp
+++ tools/python/src/dlib.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2013  Davis E. King (davis@dlib.net)
+// Copyright (C) 2015 Davis E. King (davis@dlib.net)
 // License: Boost Software License   See LICENSE.txt for the full license.
 
 #include <boost/python.hpp>
@@ -14,6 +14,7 @@ void bind_cca();
 void bind_sequence_segmenter();
 void bind_svm_struct();
 void bind_image_classes();
+void bind_rectangles();
 void bind_object_detection();
 void bind_shape_predictors();
 
@@ -38,6 +39,7 @@ BOOST_PYTHON_MODULE(dlib)
     bind_sequence_segmenter();
     bind_svm_struct();
     bind_image_classes();
+    bind_rectangles();
     bind_object_detection();
     bind_shape_predictors();
 #ifndef DLIB_NO_GUI_SUPPORT
