ab4920a4cb97edcd989b8002aeae8dc04a3dde4e
Davis King <davis@dlib.net>
2015-08-11 06:51:35 -0400

    added missing #include

--- examples/webcam_face_pose_ex.cpp
+++ examples/webcam_face_pose_ex.cpp
@@ -31,6 +31,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <dlib/image_processing/frontal_face_detector.h>
 #include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
 #include <dlib/gui_widgets.h>
 
 using namespace dlib;
