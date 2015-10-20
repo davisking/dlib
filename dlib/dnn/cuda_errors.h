fe8b99f1a8a9ca1ab2f4c43b534b4db97e1223d9
Davis King <davis@dlib.net>
2015-10-20 08:43:22 -0400

    Added comments

--- dlib/dnn/cuda_errors.h
+++ dlib/dnn/cuda_errors.h
@@ -10,11 +10,24 @@ namespace dlib
 {
     struct cuda_error : public error
     {
+        /*!
+            WHAT THIS OBJECT REPRESENTS
+                This is the exception thrown if any calls to the NVIDIA CUDA runtime
+                returns an error.  
+        !*/
+
         cuda_error(const std::string& message): error(message) {}
     };
 
+
     struct cudnn_error : public cuda_error
     {
+        /*!
+            WHAT THIS OBJECT REPRESENTS
+                This is the exception thrown if any calls to the NVIDIA cuDNN library
+                returns an error.  
+        !*/
+
         cudnn_error(const std::string& message): cuda_error(message) {}
     };
 }
