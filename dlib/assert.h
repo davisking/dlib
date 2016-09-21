35b3e65b3769ba7ec7152c26f04250b0bead868b
Ehsan Azar <dashesy@gmail.com>
2016-09-21 16:36:24 -0700

    first check large number in elsif (#239)

--- dlib/assert.h
+++ dlib/assert.h
@@ -37,10 +37,10 @@
 #elif defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 2)) && defined(__GXX_EXPERIMENTAL_CXX0X__) 
 #   define DLIB_HAS_RVALUE_REFERENCES
 #   define DLIB_HAS_INITIALIZER_LISTS
-#elif defined(_MSC_VER) && _MSC_VER >= 1600
-#   define DLIB_HAS_RVALUE_REFERENCES
 #elif defined(_MSC_VER) && _MSC_VER >= 1800
 #   define DLIB_HAS_INITIALIZER_LISTS
+#   define DLIB_HAS_RVALUE_REFERENCES
+#elif defined(_MSC_VER) && _MSC_VER >= 1600
 #   define DLIB_HAS_RVALUE_REFERENCES
 #elif defined(__INTEL_COMPILER) && defined(BOOST_INTEL_STDCXX0X)
 #   define DLIB_HAS_RVALUE_REFERENCES
