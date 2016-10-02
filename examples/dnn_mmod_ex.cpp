482b3a61b1287fa81586a6cd89fcd18e6bfd6e3d
Davis King <davis@dlib.net>
2016-10-02 12:54:14 -0400

    Simplified the network definition slightly

--- examples/dnn_mmod_ex.cpp
+++ examples/dnn_mmod_ex.cpp
@@ -55,16 +55,16 @@ template <long num_filters, typename SUBNET> using con3  = con<num_filters,3,3,1
 
 // Now we can define the 8x downsampling block in terms of conv5d blocks.  We
 // also use relu and batch normalization in the standard way.
-template <unsigned long N, typename SUBNET> using downsampler  = relu<bn_con<con5d<N, relu<bn_con<con5d<N, relu<bn_con<con5d<N,SUBNET>>>>>>>>>;
+template <typename SUBNET> using downsampler  = relu<bn_con<con5d<32, relu<bn_con<con5d<32, relu<bn_con<con5d<32,SUBNET>>>>>>>>>;
 
 // The rest of the network will be 3x3 conv layers with batch normalization and
 // relu.  So we define the 3x3 block we will use here.
-template <unsigned long N, typename SUBNET> using rcon3  = relu<bn_con<con3<N,SUBNET>>>;
+template <typename SUBNET> using rcon3  = relu<bn_con<con3<32,SUBNET>>>;
 
 // Finally, we define the entire network.   The special input_rgb_image_pyramid
 // layer causes the network to operate over a spatial pyramid, making the detector
 // scale invariant.  
-using net_type  = loss_mmod<con<1,6,6,1,1,rcon3<32,rcon3<32,rcon3<32,downsampler<32,input_rgb_image_pyramid<pyramid_down<6>>>>>>>>;
+using net_type  = loss_mmod<con<1,6,6,1,1,rcon3<rcon3<rcon3<downsampler<input_rgb_image_pyramid<pyramid_down<6>>>>>>>>;
 
 // ----------------------------------------------------------------------------------------
 
