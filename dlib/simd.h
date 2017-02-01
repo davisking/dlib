a3e3c9580b74473824653bbfa1150326cd4f704a
dmiller423 <dmiller423@gmail.com>
2017-02-01 20:09:40 +0000

    Removed _vec variants, added to existing defines. simd_check.h back in place and removed from dlib/simd.h

--- dlib/simd.h
+++ dlib/simd.h
@@ -3,20 +3,10 @@
 #ifndef DLIB_SIMd_Hh_
 #define DLIB_SIMd_Hh_
 
-#include "simd/simd_check.h"
-
-
-#ifndef DLIB_HAVE_ALTIVEC
 #include "simd/simd4f.h"
 #include "simd/simd4i.h"
 #include "simd/simd8f.h"
 #include "simd/simd8i.h"
-#else
-#include "simd/simd4f_vec.h"
-#include "simd/simd4i_vec.h"
-#include "simd/simd8f_vec.h"
-#include "simd/simd8i_vec.h"
-#endif
 
 #endif // DLIB_SIMd_Hh_
 
