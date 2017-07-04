9cd06ce3725f0ad7f8af57497e8932e06966074d
Davis King <davis@dlib.net>
2017-07-04 08:52:12 -0400

    Clarified spec slightly.

--- dlib/dnn/tensor_abstract.h
+++ dlib/dnn/tensor_abstract.h
@@ -33,9 +33,9 @@ namespace dlib
                 Finally, the convention in dlib code is to interpret the tensor as a set of
                 num_samples() 3D arrays, each of dimension k() by nr() by nc().  Also,
                 while this class does not specify a memory layout, the convention is to
-                assume that indexing into an element at coordinates (sample,k,nr,nc) can be
+                assume that indexing into an element at coordinates (sample,k,r,c) can be
                 accomplished via:
-                    host()[((sample*t.k() + k)*t.nr() + nr)*t.nc() + nc]
+                    host()[((sample*t.k() + k)*t.nr() + r)*t.nc() + c]
 
             THREAD SAFETY
                 Instances of this object are not thread-safe.  So don't touch one from
