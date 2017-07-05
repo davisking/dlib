26524fe7a11722967850aaebd68badfa2ca67dbb
Davis King <davis@dlib.net>
2017-07-05 12:12:43 -0400

    Renamed a few things to make the code and messages clearer.

--- dlib/cmake_utils/check_if_neon_available.cmake
+++ dlib/cmake_utils/check_if_neon_available.cmake
@@ -3,20 +3,18 @@
 cmake_minimum_required(VERSION 2.8.12)
 
 # Don't rerun this script if its already been executed.
-if (DEFINED COMPILER_ON_ARM_NEON)
+if (DEFINED ARM_NEON_IS_AVAILABLE)
    return()
 endif()
 
 # Set to false unless we find out otherwise in the code below.
-set(COMPILER_ON_ARM_NEON 0)
+set(ARM_NEON_IS_AVAILABLE 0)
 
 # test if __ARM_NEON__ is defined
 try_compile(test_for_neon_worked ${PROJECT_BINARY_DIR}/neon_test_build ${CMAKE_CURRENT_LIST_DIR}/test_for_neon 
 	neon_test)
 
-message ("ARM NEON TEST OK: ${test_for_neon_worked}")
-
 if(test_for_neon_worked)
-	message ("__ARM_NEON__ defined.")
-	set(COMPILER_ON_ARM_NEON 1)
+	message (STATUS "__ARM_NEON__ defined.")
+	set(ARM_NEON_IS_AVAILABLE 1)
 endif()
