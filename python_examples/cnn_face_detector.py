bbf3d987b2d172add17cd366f09cfa9400693693
Guillaume Ramé <guigzzz@users.noreply.github.com>
2017-08-27 23:40:31 +0100

    improvements to cnn face detection python interface (#780)
    * improvements to cnn face detection interface
    
    * mmod rectangle object renaming. possibility to set batch size in multi image detection. Added check to make sure images are all the same size.

--- python_examples/cnn_face_detector.py
+++ python_examples/cnn_face_detector.py
@@ -39,7 +39,6 @@
 #   Or downloaded from http://scikit-image.org/download.html.
 
 import sys
-
 import dlib
 from skimage import io
 
@@ -51,7 +50,7 @@ if len(sys.argv) < 3:
         "    http://dlib.net/files/mmod_human_face_detector.dat.bz2")
     exit()
 
-cnn_face_detection_model = dlib.cnn_face_detection_model_v1(sys.argv[1])
+cnn_face_detector = dlib.cnn_face_detection_model_v1(sys.argv[1])
 win = dlib.image_window()
 
 for f in sys.argv[2:]:
@@ -60,13 +59,27 @@ for f in sys.argv[2:]:
     # The 1 in the second argument indicates that we should upsample the image
     # 1 time.  This will make everything bigger and allow us to detect more
     # faces.
-    dets = cnn_face_detection_model.cnn_face_detector(img, 1)
+    dets = cnn_face_detector(img, 1)
+    '''
+    This detector returns a mmod_rectangles object. This object contains a list of mmod_rectangle objects.
+    These objects can be accessed by simply iterating over the mmod_rectangles object
+    The mmod_rectangle object has two member variables, a dlib.rectangle object, and a confidence score.
+    
+    It is also possible to pass a list of images to the detector.
+        - like this: dets = cnn_face_detector([image list], upsample_num, batch_size = 128)
+
+    In this case it will return a mmod_rectangless object.
+    This object behaves just like a list of lists and can be iterated over.
+    '''
     print("Number of faces detected: {}".format(len(dets)))
     for i, d in enumerate(dets):
-        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-            i, d.left(), d.top(), d.right(), d.bottom()))
+        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {} Confidence: {}".format(
+            i, d.rect.left(), d.rect.top(), d.rect.right(), d.rect.bottom(), d.confidence))
+
+    rects = dlib.rectangles()
+    rects.extend([d.rect for d in dets])
 
     win.clear_overlay()
     win.set_image(img)
-    win.add_overlay(dets)
+    win.add_overlay(rects)
     dlib.hit_enter_to_continue()
