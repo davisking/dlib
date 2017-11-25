23afdc1209f7ba41998074ed07734c0c152c2b50
Vision Workz <visionworkz@gmail.com>
2017-11-25 08:03:04 +0800

    Exposed jitter_image in Python and added an example

---
+++ python_examples/face_jitter.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+# The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
+#
+#   This example shows how faces are jittered and data augmentation using dlib's disturb_colors
+#   takes place during the training of a face recognition model using metric learning.
+#
+# COMPILING/INSTALLING THE DLIB PYTHON INTERFACE
+#   You can install dlib using the command:
+#       pip install dlib
+#
+#   Alternatively, if you want to compile dlib yourself then go into the dlib
+#   root folder and run:
+#       python setup.py install
+#   or
+#       python setup.py install --yes USE_AVX_INSTRUCTIONS
+#   if you have a CPU that supports AVX instructions, since this makes some
+#   things run faster.  This code will also use CUDA if you have CUDA and cuDNN
+#   installed.
+#
+#   Compiling dlib should work on any operating system so long as you have
+#   CMake and boost-python installed.  On Ubuntu, this can be done easily by
+#   running the command:
+#       sudo apt-get install libboost-python-dev cmake
+#
+#   Also note that this example requires OpenCV and Numpy which can be installed
+#   via the command:
+#       pip install opencv-python numpy
+#   Or downloaded from http://opencv.org/releases.html
+#
+#   The image file used in this example is in the public domain:
+#   https://commons.wikimedia.org/wiki/File:Tom_Cruise_avp_2014_4.jpg
+import sys
+
+import dlib
+import cv2
+import numpy as np
+
+def show_jittered_images(jittered_images):
+    '''
+        Shows the specified jittered images one by one
+    '''
+    for img in jittered_images:
+        cv_rgb_image = np.array(img).astype(np.uint8)
+        cv_bgr_img = cv2.cvtColor(cv_rgb_image, cv2.COLOR_RGB2BGR)
+        cv2.imshow('image',cv_bgr_img)
+        cv2.waitKey(0)
+
+if len(sys.argv) != 2:
+    print(
+        "Call this program like this:\n"
+        "   ./face_jitter.py shape_predictor_5_face_landmarks.dat\n"
+        "You can download a trained facial shape predictor from:\n"
+        "    http://dlib.net/files/shape_predictor_5_face_landmarks.dat.bz2\n")
+    exit()
+
+predictor_path = sys.argv[1]
+face_file_path = "../examples/faces/Tom_Cruise_avp_2014_4.jpg"
+
+# Load all the models we need: a detector to find the faces, a shape predictor
+# to find face landmarks so we can precisely localize the face
+detector = dlib.get_frontal_face_detector()
+sp = dlib.shape_predictor(predictor_path)
+
+# Load the image using OpenCV
+bgr_img = cv2.imread(face_file_path)
+if bgr_img is None:
+    print("Sorry, we could not load '{}' as an image".format(face_file_path))
+    exit()
+
+# Convert to RGB since dlib uses RGB images
+img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
+
+# Ask the detector to find the bounding boxes of each face.
+dets = detector(img)
+
+num_faces = len(dets)
+
+# Find the 5 face landmarks we need to do the alignment.
+faces = dlib.full_object_detections()
+for detection in dets:
+    faces.append(sp(img, detection))
+
+# Get the aligned face image and show it
+image = dlib.get_face_chip(img, faces[0], size=320)
+cv_rgb_image = np.array(image).astype(np.uint8)
+cv_bgr_img = cv2.cvtColor(cv_rgb_image, cv2.COLOR_RGB2BGR)
+cv2.imshow('image',cv_bgr_img)
+cv2.waitKey(0)
+
+# Show 5 jittered images without data augmentation
+jittered_images = dlib.jitter_image(cv_rgb_image, num_jitters=5)
+show_jittered_images(jittered_images)
+
+# Show 5 jittered images with data augmentation
+jittered_images = dlib.jitter_image(cv_rgb_image, num_jitters=5, disturb_colors=True)
+show_jittered_images(jittered_images)
+cv2.destroyAllWindows()
--- tools/python/src/image.cpp
+++ tools/python/src/image.cpp
@@ -1,6 +1,7 @@
 #include <dlib/python.h>
 #include <boost/python/args.hpp>
 #include "dlib/pixel.h"
+#include <dlib/image_transforms.h>
 
 using namespace dlib;
 using namespace std;
@@ -25,10 +26,61 @@ string print_rgb_pixel_repr(const rgb_pixel& p)
 }
 
 // ----------------------------------------------------------------------------------------
+
+boost::python::list get_jitter_images(object img, size_t num_jitters = 1, bool disturb_colors = false)
+{
+    if (!is_rgb_python_image(img))
+        throw dlib::error("Unsupported image type, must be RGB image.");
+
+    dlib::rand rnd;
+
+    // Convert the image to matrix<rgb_pixel> for processing
+    matrix<rgb_pixel> img_mat;
+    assign_image(img_mat, numpy_rgb_image(img));
+
+    // The top level list (containing 1 or more images) to return to python
+    boost::python::list jitter_list;
+
+    for (int i = 0; i < num_jitters; ++i) {
+        // Get a jittered crop
+        matrix<rgb_pixel> crop = dlib::jitter_image(img_mat,rnd);
+        // If required disturb colors of the image
+        if(disturb_colors)
+            dlib::disturb_colors(crop,rnd);
+
+        // Treat the image as a list of lists
+        boost::python::list img_as_list;
+        size_t rows = img_mat.nr();
+        size_t cols = img_mat.nc();
+        
+        for(size_t row=0; row<rows; row++) {
+            boost::python::list row_list;
+            for(size_t col=0; col<cols; col++) {
+                rgb_pixel pixel = crop(row, col);
+                boost::python::list item;
+                
+                item.append(pixel.red);
+                item.append(pixel.green);
+                item.append(pixel.blue);
+                row_list.append(item);
+            }
+            img_as_list.append(row_list);
+        }
+
+        jitter_list.append(img_as_list);
+    }
+        
+    return jitter_list;
+}
+
+BOOST_PYTHON_FUNCTION_OVERLOADS(get_jitter_images_with_defaults, get_jitter_images, 1, 3)
+
+// ----------------------------------------------------------------------------------------
 void bind_image_classes()
 {
     using boost::python::arg;
 
+    {
     class_<rgb_pixel>("rgb_pixel")
         .def(init<unsigned char,unsigned char,unsigned char>( (arg("red"),arg("green"),arg("blue")) ))
         .def("__str__", &print_rgb_pixel_str)
@@ -36,4 +88,12 @@ void bind_image_classes()
         .add_property("red", &rgb_pixel::red)
         .add_property("green", &rgb_pixel::green)
         .add_property("blue", &rgb_pixel::blue);
+    }
+
+    def("jitter_image", &get_jitter_images, get_jitter_images_with_defaults(
+    "Takes an image and returns a list of jittered images."
+    "The returned list contains num_jitters images (default is 1)."
+    "If disturb_colors is set to True, the colors of the image are disturbed (default is False)", 
+    (arg("img"), arg("num_jitters"), arg("disturb_colors"))
+    ));
 }
