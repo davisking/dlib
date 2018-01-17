e1215e5ce53d72f01bd96839f36ebeff218d23e7
Davis King <davis@dlib.net>
2018-01-17 06:44:08 -0500

    fix travis build and test script

--- dlib/travis/build-and-test.sh
+++ dlib/travis/build-and-test.sh
@@ -3,14 +3,17 @@
 set -eux
 
 # build dlib and tests
-mkdir build
-cd build
 if [ "$VARIANT" = "test" ]; then
+  mkdir build
+  cd build
   ../cmake/bin/cmake ../dlib/test -DCMAKE_BUILD_TYPE=Release
   ../cmake/bin/cmake --build . --target dtest -- -j 2
   ./dtest --runall
 fi
+
 if [ "$VARIANT" = "examples" ]; then
+  mkdir build
+  cd build
   ../cmake/bin/cmake ../examples -DCMAKE_BUILD_TYPE=Release
   ../cmake/bin/cmake --build . -- -j 1
 fi
