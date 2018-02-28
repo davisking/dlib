feaf245dec5094d21597c1ad8e751b4495ddcff5
Davis King <davis@dlib.net>
2018-02-28 07:12:51 -0500

    Removed deprecated files.

--- dlib/all_console.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2006  Davis E. King (davis@dlib.net)
-// License: Boost Software License   See LICENSE.txt for the full license.
-#ifndef DLIB_ALL_CONSOLe_
-#define DLIB_ALL_CONSOLe_
-
-#error "This file has been replaced.  Instead you should add dlib/all/source.cpp to your project"
-
-#endif // DLIB_ALL_CONSOLe_
-
