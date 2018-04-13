4104c2cd198f44a79a58dc17f31cbfda62e7dca8
Davis King <davis@dlib.net>
2018-04-13 09:45:19 -0400

    Updated windows test bat file.

--- dlib/test/WINDOWS_build_and_run_all_unit_tests.bat
+++ dlib/test/WINDOWS_build_and_run_all_unit_tests.bat
@@ -9,8 +9,8 @@ echo testing python >> test_log.txt
 rmdir /S /Q build_python
 mkdir build_python
 cd build_python
-cmake -G "Visual Studio 14 2015 Win64" ../../../tools/python  -DPYTHON3=ON
-cmake --build . --config Release --target install || exit /B
+cmake -G "Visual Studio 14 2015 Win64" -T host=x64 ../../../tools/python  
+cmake --build . --config Release || exit /B
 ping 127.0.0.1 -n 5 -w 1000 > null
 cd ..
 
@@ -20,7 +20,7 @@ echo testing vc2015 >> test_log.txt
 rmdir /S /Q build_vc2015_64
 mkdir build_vc2015_64
 cd build_vc2015_64
-cmake -G "Visual Studio 14 2015 Win64" .. 
+cmake -G "Visual Studio 14 2015 Win64" -T host=x64 .. 
 cmake --build . --config Release || exit /B
 ping 127.0.0.1 -n 5 -w 1000 > null
 cmake --build . --config Debug || exit /B
