940546f8789dd7b205ab4c0ce449dc31312aa4b5
Davis King <davis@dlib.net>
2018-05-07 07:24:49 -0400

    Make python docs include __call__ and __init__

--- docs/docs/python/conf.py
+++ docs/docs/python/conf.py
@@ -244,3 +244,14 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
+
+
+
+# Make sphinx document constructors and __call__
+def skip(app, what, name, obj, skip, options):
+    if name == "__init__" or name == "__call__":
+        return False
+    return skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
