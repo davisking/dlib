104ffddb4c3596edf841a69f80f386d15aa8628e
Davis King <davis@dlib.net>
2018-05-21 21:21:40 -0400

    Make python extension automatically enable AVX instructions if the host machine supports them.

---
+++ dlib/cmake_utils/check_if_avx_instructions_executable_on_host.cmake
@@ -0,0 +1,19 @@
+# This script checks if your compiler and host processor can generate and then run programs with AVX instructions.
+
+cmake_minimum_required(VERSION 2.8.12)
+
+# Don't rerun this script if its already been executed.
+if (DEFINED AVX_IS_AVAILABLE_ON_HOST)
+   return()
+endif()
+
+# Set to false unless we find out otherwise in the code below.
+set(AVX_IS_AVAILABLE_ON_HOST 0)
+
+try_compile(test_for_avx_worked ${PROJECT_BINARY_DIR}/avx_test_build ${CMAKE_CURRENT_LIST_DIR}/test_for_avx 
+	avx_test)
+
+if(test_for_avx_worked)
+	message (STATUS "AVX instructions can be executed by the host processor.")
+	set(AVX_IS_AVAILABLE_ON_HOST 1)
+endif()
