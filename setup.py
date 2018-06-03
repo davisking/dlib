106c905b7fbda2348a2f7014ce37076c8198952b
Davis King <davis@dlib.net>
2018-06-03 10:53:04 -0400

    Added a comment

--- setup.py
+++ setup.py
@@ -147,7 +147,8 @@ class CMakeBuild(build_ext):
             cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
             if sys.maxsize > 2**32:
                 cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
+            # Do a parallel build
+            build_args += ['--', '/m'] 
         else:
             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
             # Do a parallel build
