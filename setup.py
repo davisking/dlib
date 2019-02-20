16a1056916adadeffa3c6554b7470aaff5e83ae2
Davis King <davis@dlib.net>
2019-02-20 07:54:42 -0500

    Removing the install_requires cmake from setup.py because the pip cmake package is hopelessly busted at the moment.

--- setup.py
+++ setup.py
@@ -231,7 +231,7 @@ setup(
     cmdclass=dict(build_ext=CMakeBuild, test=PyTest),
     zip_safe=False,
     tests_require=['pytest==3.8'],
-    install_requires=['cmake'],
+    #install_requires=['cmake'], # removed because the pip cmake package is busted, maybe someday it will be usable.
     packages=['dlib'],
     keywords=['dlib', 'Computer Vision', 'Machine Learning'],
     classifiers=[
