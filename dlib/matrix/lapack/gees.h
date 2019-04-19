d0cf56ad39c93ab3a1d28e3aeb8c07f0b61d1c22
Edoardo Morandi <morandidodo@gmail.com>
2019-04-19 09:55:30 +0200

    Fixed constness for lapack functions

--- dlib/matrix/lapack/gees.h
+++ dlib/matrix/lapack/gees.h
@@ -21,15 +21,15 @@ namespace dlib
 
             extern "C"
             {
-                void DLIB_FORTRAN_ID(dgees) (char *jobvs, char *sort, L_fp select, integer *n, 
-                                             double *a, integer *lda, integer *sdim, double *wr, 
-                                             double *wi, double *vs, integer *ldvs, double *work, 
-                                             integer *lwork, logical *bwork, integer *info);
+                void DLIB_FORTRAN_ID(dgees) (const char *jobvs, const char *sort, const L_fp select, const integer *n, 
+                                             double *a, const integer *lda, integer *sdim, double *wr, 
+                                             double *wi, double *vs, const integer *ldvs, double *work, 
+                                             const integer *lwork, logical *bwork, integer *info);
 
-                void DLIB_FORTRAN_ID(sgees) (char *jobvs, char *sort, L_fp select, integer *n, 
-                                             float *a, integer *lda, integer *sdim, float *wr, 
-                                             float *wi, float *vs, integer *ldvs, float *work, 
-                                             integer *lwork, logical *bwork, integer *info);
+                void DLIB_FORTRAN_ID(sgees) (const char *jobvs, const char *sort, const L_fp select, const integer *n, 
+                                             float *a, const integer *lda, integer *sdim, float *wr, 
+                                             float *wi, float *vs, const integer *ldvs, float *work, 
+                                             const integer *lwork, logical *bwork, integer *info);
 
             }
 
