3bb08a409c0bee1ebf14038cd2242bc13fd6207f
VirgileD <vdevaux@gmail.com>
2019-05-16 22:24:09 +0200

    Removes "__inline__" define in jconfig, broke VC build

--- dlib/external/libjpeg/jconfig.h
+++ dlib/external/libjpeg/jconfig.h
@@ -19,7 +19,6 @@
 #ifdef JPEG_INTERNALS
 
 #undef RIGHT_SHIFT_IS_UNSIGNED
-#define INLINE __inline__
 /* These are for configuring the JPEG memory manager. */
 #undef DEFAULT_MAX_MEM
 #undef NO_MKTEMP
