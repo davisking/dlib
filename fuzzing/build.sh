14900d5b2e1170bff97ffc500bfc1a4362ecba94
Paul Dreik <github@pauldreik.se>
2019-07-18 20:45:41 +0200

    build a faster fuzzer as well as the sanitizer one.

--- fuzzing/build.sh
+++ fuzzing/build.sh
@@ -13,7 +13,7 @@ echo $me: root=$root
 
 here=$(pwd)
 
-CXXFLAGSALL="-DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION= -g -std=c++1z"
+CXXFLAGSALL="-DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION= -g -std=c++1z -O3"
 CMAKEFLAGSALL="$root -GNinja -DCMAKE_BUILD_TYPE=Debug"
 
 #builds fuzzers for local fuzzing with libfuzzer with asan+usan
@@ -23,6 +23,19 @@ cd $builddir
 CXX="clang++-7" \
 CXXFLAGS="$CXXFLAGSALL -fsanitize=fuzzer-no-link,address,undefined" \
 cmake $CMAKEFLAGSALL -DCMAKE_BUILD_TYPE=Debug $fuzzdir
+
+echo $me: building
+cmake --build $builddir
+
+
+
+#builds fuzzers for local fuzzing with libfuzzer without sanitizers
+builddir=$here/build-fuzzers-fast
+mkdir -p $builddir
+cd $builddir
+CXX="clang++-7" \
+CXXFLAGS="$CXXFLAGSALL -fsanitize=fuzzer-no-link" \
+cmake $CMAKEFLAGSALL -DCMAKE_BUILD_TYPE=Release $fuzzdir
 
 echo $me: building
 cmake --build $builddir
