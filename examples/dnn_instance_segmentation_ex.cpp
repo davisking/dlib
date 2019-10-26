df490923cbf8affd436b0997e988b5223f245350
Juha Reunanen <juha.reunanen@tomaattinen.com>
2019-10-26 20:42:04 +0300

    Inference - first version, at least compiles!

--- examples/dnn_instance_segmentation_ex.cpp
+++ examples/dnn_instance_segmentation_ex.cpp
@@ -34,76 +34,6 @@ using namespace dlib;
  
 // ----------------------------------------------------------------------------------------
 
-// The PASCAL VOC2012 dataset contains 20 ground-truth classes + background.  Each class
-// is represented using an RGB color value.  We associate each class also to an index in the
-// range [0, 20], used internally by the network.  To generate nice RGB representations of
-// inference results, we need to be able to convert the index values to the corresponding
-// RGB values.
-
-// Given an index in the range [0, 20], find the corresponding PASCAL VOC2012 class
-// (e.g., 'dog').
-const Voc2012class& find_voc2012_class(const uint16_t& index_label)
-{
-    return find_voc2012_class(
-        [&index_label](const Voc2012class& voc2012class)
-        {
-            return index_label == voc2012class.index;
-        }
-    );
-}
-
-// Convert an index in the range [0, 20] to a corresponding RGB class label.
-inline rgb_pixel index_label_to_rgb_label(uint16_t index_label)
-{
-    return find_voc2012_class(index_label).rgb_label;
-}
-
-// Convert an image containing indexes in the range [0, 20] to a corresponding
-// image containing RGB class labels.
-void index_label_image_to_rgb_label_image(
-    const matrix<uint16_t>& index_label_image,
-    matrix<rgb_pixel>& rgb_label_image
-)
-{
-    const long nr = index_label_image.nr();
-    const long nc = index_label_image.nc();
-
-    rgb_label_image.set_size(nr, nc);
-
-    for (long r = 0; r < nr; ++r)
-    {
-        for (long c = 0; c < nc; ++c)
-        {
-            rgb_label_image(r, c) = index_label_to_rgb_label(index_label_image(r, c));
-        }
-    }
-}
-
-// Find the most prominent class label from amongst the per-pixel predictions.
-std::string get_most_prominent_non_background_classlabel(const matrix<uint16_t>& index_label_image)
-{
-    const long nr = index_label_image.nr();
-    const long nc = index_label_image.nc();
-
-    std::vector<unsigned int> counters(class_count);
-
-    for (long r = 0; r < nr; ++r)
-    {
-        for (long c = 0; c < nc; ++c)
-        {
-            const uint16_t label = index_label_image(r, c);
-            ++counters[label];
-        }
-    }
-
-    const auto max_element = std::max_element(counters.begin() + 1, counters.end());
-    const uint16_t most_prominent_index_label = max_element - counters.begin();
-
-    return find_voc2012_class(most_prominent_index_label).classlabel;
-}
-
-// ----------------------------------------------------------------------------------------
-
 int main(int argc, char** argv) try
 {
     if (argc != 2)
@@ -118,20 +48,21 @@ int main(int argc, char** argv) try
         return 1;
     }
 
-    // Read the file containing the trained network from the working directory.
-    anet_type net;
-    deserialize(instance_segmentation_net_filename) >> net;
+    // Read the file containing the trained networks from the working directory.
+    det_anet_type det_net;
+    seg_anet_type seg_net;
+    deserialize(instance_segmentation_net_filename) >> det_net >> seg_net;
 
     // Show inference results in a window.
     image_window win;
 
     matrix<rgb_pixel> input_image;
-    matrix<uint16_t> index_label_image;
-    matrix<rgb_pixel> rgb_label_image;
 
     // Find supported image files.
     const std::vector<file> files = dlib::get_files_in_directory_tree(argv[1],
         dlib::match_endings(".jpeg .jpg .png"));
+
+    dlib::rand rnd;
 
     cout << "Found " << files.size() << " images, processing..." << endl;
 
@@ -140,28 +71,51 @@ int main(int argc, char** argv) try
         // Load the input image.
         load_image(input_image, file.full_name());
 
-        // Create predictions for each pixel. At this point, the type of each prediction
-        // is an index (a value between 0 and 20). Note that the net may return an image
-        // that is not exactly the same size as the input.
-        const matrix<uint16_t> temp = net(input_image);
+        // Find instances in the input image
+        const auto instances = det_net(input_image);
 
-        // Crop the returned image to be exactly the same size as the input.
-        const chip_details chip_details(
-            centered_rect(temp.nc() / 2, temp.nr() / 2, input_image.nc(), input_image.nr()),
-            chip_dims(input_image.nr(), input_image.nc())
-        );
-        extract_image_chip(temp, chip_details, index_label_image, interpolate_nearest_neighbor());
+        matrix<rgb_pixel> rgb_label_image;
+        matrix<rgb_pixel> input_chip;
 
-        // Convert the indexes to RGB values.
-        index_label_image_to_rgb_label_image(index_label_image, rgb_label_image);
+        rgb_label_image = rgb_pixel(0, 0, 0);
+
+        for (const auto& instance : instances)
+        {
+            const chip_details chip_details(instance.rect, chip_dims(seg_dim, seg_dim));
+            extract_image_chip(input_image, chip_details, input_chip, interpolate_bilinear());
+
+            const auto mask = seg_net(input_chip);
+
+            rgb_pixel random_color(
+                rnd.get_random_8bit_number(),
+                rnd.get_random_8bit_number(),
+                rnd.get_random_8bit_number()
+            );
+
+            dlib::matrix<uint16_t> resized_mask(
+                static_cast<int>(chip_details.rect.height()),
+                static_cast<int>(chip_details.rect.width())
+            );
+
+            dlib::resize_image(mask, resized_mask);
+
+            for (int r = 0; r < resized_mask.nr(); ++r)
+                for (int c = 0; c < resized_mask.nc(); ++c)
+                    if (resized_mask(r, c))
+                    {
+                        const auto y = chip_details.rect.top() + r;
+                        const auto x = chip_details.rect.left() + c;
+                        if (y >= 0 && y < rgb_label_image.nr() && x >= 0 && x < rgb_label_image.nc())
+                            rgb_label_image(y, x) = random_color;
+                    }
+
+            dlib::draw_rectangle(rgb_label_image, instance.rect, dlib::rgb_pixel(255, 255, 255), 1);
+        }
 
         // Show the input image on the left, and the predicted RGB labels on the right.
         win.set_image(join_rows(input_image, rgb_label_image));
 
-        // Find the most prominent class label from amongst the per-pixel predictions.
-        const std::string classlabel = get_most_prominent_non_background_classlabel(index_label_image);
-
-        cout << file.name() << " : " << classlabel << " - hit enter to process the next image";
+        cout << file.name() << " - hit enter to process the next image";
         cin.get();
     }
 }
