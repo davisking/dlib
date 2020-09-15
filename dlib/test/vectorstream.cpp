9a7c70f8db2be991904b97e27e5ff66d77758f58
pf <pf@pf-ubuntu-dev>
2020-09-15 08:14:06 +0100

    [DLIB] vectorstream unit test is updated to run identical tests on dlib::vectorstream& and std::iostream&

--- dlib/test/vectorstream.cpp
+++ dlib/test/vectorstream.cpp
@@ -21,6 +21,116 @@ namespace
 
 
     logger dlog("test.vectorstream");
+    
+    namespace helper 
+    {        
+        /*
+        * This class helps test the functionality of dlib::vectorstream when an object reference is treated as:
+        * - dlib::vectorstream&
+        * - std::iostream&
+        * Depending on which is used, the underlying function calls might have different call stacks.
+        */
+        struct test1_helper
+        {
+            test1_helper(
+                std::vector<char>& buf_
+            ) : buf(buf_)
+            {}
+
+            template <typename T>
+            void operator()(T& s)
+            {
+                for (int i = -1000; i <= 1000; ++i)
+                {
+                    char ch = i;
+                    s.put(ch);
+                }
+
+                DLIB_TEST(buf.size() == 2001);
+
+                int cnt = -1000;
+                for (unsigned long i = 0; i < buf.size(); ++i)
+                {
+                    char ch = cnt;
+                    DLIB_TEST(buf[i] == ch);
+                    ++cnt;
+                }
+
+                for (int i = -1000; i <= 1000; ++i)
+                {
+                    DLIB_TEST(s.peek() != EOF);
+                    char ch1 = i;
+                    char ch2 = s.get();
+                    DLIB_TEST(ch1 == ch2);
+                }
+
+                DLIB_TEST(s.peek() == EOF);
+                DLIB_TEST(s.get() == EOF);
+
+                s.clear();
+                s.seekg(6);
+
+                for (int i = -1000+6; i <= 1000; ++i)
+                {
+                    DLIB_TEST(s.peek() != EOF);
+                    char ch1 = i;
+                    char ch2 = s.get();
+                    DLIB_TEST(ch1 == ch2);
+                }
+
+                DLIB_TEST(s.peek() == EOF);
+                DLIB_TEST(s.get() == EOF);
+
+                std::string temp;
+                temp = "one two three!";
+
+                s.clear();
+                s.seekg(0);
+                buf.clear();
+
+                serialize(temp, s);
+                std::string temp2;
+                deserialize(temp2, s);
+                DLIB_TEST(temp2 == temp);
+
+                s.put('1');
+                s.put('2');
+                s.put('3');
+                s.put('4');
+                DLIB_TEST(s.get() == '1');
+                DLIB_TEST(s.get() == '2');
+                DLIB_TEST(s.get() == '3');
+                DLIB_TEST(s.get() == '4');
+
+                s.putback('4');
+                DLIB_TEST(s.get() == '4');
+                s.putback('4');
+                s.putback('3');
+                s.putback('2');
+                s.putback('1');
+                DLIB_TEST(s.get() == '1');
+                DLIB_TEST(s.get() == '2');
+                DLIB_TEST(s.get() == '3');
+                DLIB_TEST(s.get() == '4');
+                DLIB_TEST(s.good() == true);
+                DLIB_TEST(s.get() == EOF);
+                DLIB_TEST(s.good() == false);
+
+                // make sure seeking to a crazy offset doesn't mess things up
+                s.clear();
+                s.seekg(1000000);
+                DLIB_TEST(s.get() == EOF);
+                DLIB_TEST(s.good() == false);
+                s.clear();
+                s.seekg(1000000);
+                char sbuf[100];
+                s.read(sbuf, sizeof(sbuf));
+                DLIB_TEST(s.good() == false);
+            }
+
+            std::vector<char>& buf;
+        };
+    }
 
 // ----------------------------------------------------------------------------------------
 
@@ -28,95 +138,20 @@ namespace
     {
         print_spinner();
 
-        std::vector<char> buf;
-        vectorstream s(buf);
-
-        for (int i = -1000; i <= 1000; ++i)
         {
-            char ch = i;
-            s.put(ch);
+            std::vector<char> buf;
+            vectorstream s1(buf);
+            helper::test1_helper t(buf);
+            t(s1);
         }
-
-        DLIB_TEST(buf.size() == 2001);
-
-        int cnt = -1000;
-        for (unsigned long i = 0; i < buf.size(); ++i)
+        
         {
-            char ch = cnt;
-            DLIB_TEST(buf[i] == ch);
-            ++cnt;
-        }
-
-        for (int i = -1000; i <= 1000; ++i)
-        {
-            DLIB_TEST(s.peek() != EOF);
-            char ch1 = i;
-            char ch2 = s.get();
-            DLIB_TEST(ch1 == ch2);
-        }
-
-        DLIB_TEST(s.peek() == EOF);
-        DLIB_TEST(s.get() == EOF);
-
-        s.clear();
-        s.seekg(6);
-
-        for (int i = -1000+6; i <= 1000; ++i)
-        {
-            DLIB_TEST(s.peek() != EOF);
-            char ch1 = i;
-            char ch2 = s.get();
-            DLIB_TEST(ch1 == ch2);
-        }
-
-        DLIB_TEST(s.peek() == EOF);
-        DLIB_TEST(s.get() == EOF);
-
-        std::string temp;
-        temp = "one two three!";
-
-        s.seekg(0);
-        buf.clear();
-        s.clear();
-
-        serialize(temp, s);
-        std::string temp2;
-        deserialize(temp2, s);
-        DLIB_TEST(temp2 == temp);
-
-        s.put('1');
-        s.put('2');
-        s.put('3');
-        s.put('4');
-        DLIB_TEST(s.get() == '1');
-        DLIB_TEST(s.get() == '2');
-        DLIB_TEST(s.get() == '3');
-        DLIB_TEST(s.get() == '4');
-
-        s.putback('4');
-        DLIB_TEST(s.get() == '4');
-        s.putback('4');
-        s.putback('3');
-        s.putback('2');
-        s.putback('1');
-        DLIB_TEST(s.get() == '1');
-        DLIB_TEST(s.get() == '2');
-        DLIB_TEST(s.get() == '3');
-        DLIB_TEST(s.get() == '4');
-        DLIB_TEST(s.good() == true);
-        DLIB_TEST(s.get() == EOF);
-        DLIB_TEST(s.good() == false);
-
-        // make sure seeking to a crazy offset doesn't mess things up
-        s.clear();
-        s.seekg(1000000);
-        DLIB_TEST(s.get() == EOF);
-        DLIB_TEST(s.good() == false);
-        s.clear();
-        s.seekg(1000000);
-        char sbuf[100];
-        s.read(sbuf, sizeof(sbuf));
-        DLIB_TEST(s.good() == false);
+            vector<char> buf;
+            dlib::vectorstream s1(buf);
+            std::iostream& s2(s1);
+            helper::test1_helper t(buf);
+            t(s2);
+        }        
     }
 
 // ----------------------------------------------------------------------------------------
