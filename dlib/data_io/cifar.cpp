c5fe50f490f9caa11f3c111ed6c098a2b037afe7
Adrià Arrufat <adria.arrufat@outlook.com>
2020-11-24 11:35:47 +0900

    Add Davis' suggestions

--- dlib/data_io/cifar.cpp
+++ dlib/data_io/cifar.cpp
@@ -10,6 +10,51 @@
 
 namespace dlib
 {
+    namespace impl
+    {
+        void load_cifar_10_batch (
+            const std::string& folder_name,
+            const std::string& batch_name,
+            const size_t first_idx,
+            const size_t images_per_batch,
+            std::vector<matrix<rgb_pixel>>& images,
+            std::vector<unsigned long>& labels
+        )
+        {
+            using namespace std;
+            ifstream fin((folder_name + "/" + batch_name).c_str(), ios::binary);
+            if (!fin) throw error("Unable to open file " + batch_name);
+            const long nr = 32;
+            const long nc = 32;
+            const long plane_size = nr * nc;
+            const long image_size = 3 * plane_size;
+
+            for (size_t i = 0; i < images_per_batch; ++i)
+            {
+                char l;
+                fin.read(&l, 1);
+                labels[first_idx + i] = l;
+                images[first_idx + i].set_size(nr, nc);
+
+                std::array<unsigned char, image_size> buffer;
+                fin.read((char*)(buffer.data()), buffer.size());
+                for (long k = 0; k < plane_size; ++k)
+                {
+                    char r = buffer[0 * plane_size + k];
+                    char g = buffer[1 * plane_size + k];
+                    char b = buffer[2 * plane_size + k];
+                    const long row = k / nr;
+                    const long col = k % nr;
+                    images[first_idx + i](row, col) = rgb_pixel(r, g, b);
+                }
+            }
+
+            if (!fin) throw error("Unable to read file " + batch_name);
+
+            if (fin.get() != EOF) throw error("Unexpected bytes at end of " + batch_name);
+        }
+    }
+
     void load_cifar_10_dataset (
         const std::string& folder_name,
         std::vector<matrix<rgb_pixel>>& training_images,
@@ -19,85 +64,42 @@ namespace dlib
     )
     {
         using namespace std;
-        std::vector<string> file_names{
+
+        const size_t images_per_batch = 10000;
+        const size_t num_training_batches = 5;
+        const size_t num_testing_batches = 1;
+
+        training_images.resize(images_per_batch * num_training_batches);
+        training_labels.resize(images_per_batch * num_training_batches);
+        testing_images.resize(images_per_batch * num_testing_batches);
+        testing_labels.resize(images_per_batch * num_testing_batches);
+
+        std::vector<string> training_batches_names{
             "data_batch_1.bin",
             "data_batch_2.bin",
             "data_batch_3.bin",
             "data_batch_4.bin",
             "data_batch_5.bin",
-            "test_batch.bin"
         };
-        std::vector<ifstream> fins;
-        for (const auto& file_name : file_names)
+
+        for (size_t i = 0; i < num_training_batches; ++i)
         {
-            fins.emplace_back((folder_name + "/" + file_name).c_str(), ios::binary);
+            impl::load_cifar_10_batch(
+                folder_name,
+                training_batches_names[i],
+                i * images_per_batch,
+                images_per_batch,
+                training_images,
+                training_labels);
         }
 
-        for (size_t i = 0; i < fins.size(); ++i)
-        {
-            if (!fins[i]) throw error("Unable to open file " + file_names[i]);
-        }
-
-        const size_t images_per_batch = 10000;
-        const size_t training_batches = 5;
-        const size_t testing_batches = 1;
-        const long nr = 32;
-        const long nc = 32;
-        const long plane_size = nr * nc;
-        const long image_size = 3 * plane_size;
-
-        training_images.resize(images_per_batch * training_batches);
-        training_labels.resize(images_per_batch * training_batches);
-        testing_images.resize(images_per_batch * testing_batches);
-        testing_labels.resize(images_per_batch * testing_batches);
-
-        for (size_t i = 0; i < fins.size(); ++i)
-        {
-            bool test_batch = i == fins.size() - 1;
-            for (size_t j = 0; j < images_per_batch; ++j)
-            {
-                auto idx = i * images_per_batch + j;
-
-                char l;
-                fins[i].read(&l, 1);
-                if (test_batch)
-                {
-                    idx -= training_images.size();
-                    testing_labels[idx] = l;
-                    testing_images[idx].set_size(nr, nc);
-                }
-                else
-                {
-                    training_labels[idx] = l;
-                    training_images[idx].set_size(nr, nc);
-                }
-
-                std::array<unsigned char, image_size> buffer;
-                fins[i].read((char*)(&buffer[0]), buffer.size());
-                for (long k = 0; k < plane_size; ++k)
-                {
-                    char r = buffer[0 * plane_size + k];
-                    char g = buffer[1 * plane_size + k];
-                    char b = buffer[2 * plane_size + k];
-                    const long row = k / nr;
-                    const long col = k % nr;
-                    if (test_batch)
-                        testing_images[idx](row, col) = rgb_pixel(r, g, b);
-                    else
-                        training_images[idx](row, col) = rgb_pixel(r, g, b);
-                }
-            }
-        }
-
-        for (size_t i = 0; i < fins.size(); ++i)
-        {
-            if (!fins[i]) throw error("Unable to read file " + file_names[i]);
-        }
-
-        for (size_t i = 0; i < fins.size(); ++i)
-        {
-            if (fins[i].get() != EOF) throw error("Unexpected bytes at end of " + file_names[i]);
-        }
+        impl::load_cifar_10_batch(
+            folder_name,
+            "test_batch.bin",
+            0,
+            images_per_batch,
+            testing_images,
+            testing_labels);
     }
 }
 // ----------------------------------------------------------------------------------------
