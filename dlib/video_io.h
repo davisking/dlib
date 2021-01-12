7313b1ee92880f583156ad64eb099fb9e3314588
pf <pf@pf-ubuntu-dev>
2021-01-12 12:37:32 +0000

    [VIDEO] beginnings of dlib::video_capture PR

---
+++ dlib/video_io.h
@@ -0,0 +1,14 @@
+// Copyright (C) 2021  Davis E. King (davis@dlib.net)
+// License: Boost Software License   See LICENSE.txt for the full license.
+
+#ifdef DLIB_ALL_SOURCE_END
+#include "dlib_basic_cpp_build_tutorial.txt"
+#endif
+
+#ifndef DLIB_VIDEO_IO_ 
+#define DLIB_VIDEO_IO_
+
+#include "video_loader/video_capture.h"
+
+#endif // DLIB_VIDEO_IO_ 
+
