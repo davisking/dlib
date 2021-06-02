2db84e571c9008f4cd9955be29ef6205e5f8082a
zbirenbaum <zacharyobirenbaum@gmail.com>
2021-06-02 17:52:02 -0400

    somewhat fixed start button call. Figure out toolkit error and pathing improvements outstanding

--- sealFindr.py
+++ sealFindr.py
@@ -1,13 +1,17 @@
 import os
 import sys
+import pathlib
+import string
 
-SEALROOT = '/Users/aylab/Documents/Hieu/DlibSealGui/'
+
 def main(args):
-
 	if len(args) != 2:
 		print("Call as: python sealFace.py YOURFOLDERNAME")
 		return
-	directory = args[1]
+	
+	directory = SEALROOT#args[1] 
+	#fix this in arg call, no hardcode bullshit, even for testing. Should be $ROOT_PATH+/path/to/dir
+	
 	arguments = ""
 	for filename in os.listdir(directory):
 		if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png") or filename.endswith(".JPG"): 
@@ -17,8 +21,10 @@ def main(args):
 		os.mkdir(chipfolder)
 	except:
 		pass
-	os.system("cd " + SEALROOT)
-	os.system("./seal.exe seal.dat " + chipfolder + " " + arguments)
+	os.system("seal.exe seal.dat " + chipfolder + " " + arguments)
 
-if __name__ == "__main__":
+if __name__ == "__main__":		
+	SEALROOT = str(pathlib.Path('data'))
+	print(SEALROOT)
 	main(sys.argv)
+	#main(SEALROOT)
