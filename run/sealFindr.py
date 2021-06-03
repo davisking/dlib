9f09c618a6426a25e9c35ad5285899e14eddda44
AyLab <aylab@olin206-98920m.colgate.edu>
2021-06-03 17:23:15 -0400

    Update gitignore

--- run/sealFindr.py
+++ run/sealFindr.py
@@ -10,26 +10,24 @@ def main(args):
 		return
 	
 	directory = SEALROOT#args[1] 
-	#fix this in arg call, no hardcode bullshit, even for testing. Should be $ROOT_PATH+/path/to/dir
+	#fix this in arg call, even for testing. Should be $ROOT_PATH+/path/to/dir
 	
 	arguments = ""
 	for filename in os.listdir(directory):
 		if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png") or filename.endswith(".JPG"): 
-			arguments += (directory + "/" + filename + " ")
+			arguments += (filename + " ")
 	chipfolder = directory+"Chips"
 	try:
 		os.mkdir(chipfolder)
 	except:
 		pass
-	#
-	#except:
-	print(os.listdir())
+
 	if str(os.name) == "posix":
-		print("test")
-		os.system("./seal.exe seal.dat " + chipfolder + " " + arguments)
+		print("MacOS")
+		os.system("./seal seal.dat " + directory + " " + arguments)
 	if str(os.name) == 'nt':
-		print('windo$e')
-		os.system("seal.exe seal.dat " + chipfolder + " " + arguments)
+		print('Window')
+		os.system("seal.exe seal.dat " + directory + " " + arguments)
 
 if __name__ == "__main__":		
 	SEALROOT = str(pathlib.Path('data'))
