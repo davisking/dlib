748a23732bb97d901dfbf2c77e7e7800bd95dd7b
AyLab <aylab@olin206-98920m.colgate.edu>
2021-06-03 18:03:21 -0400

    Unhardcode sealFindr.py

--- run/sealFindr.py
+++ run/sealFindr.py
@@ -9,8 +9,7 @@ def main(args):
 		print("Call as: python sealFace.py YOURFOLDERNAME")
 		return
 	
-	directory = SEALROOT#args[1] 
-	#fix this in arg call, even for testing. Should be $ROOT_PATH+/path/to/dir
+	directory = args
 	
 	arguments = ""
 	for filename in os.listdir(directory):
@@ -23,14 +22,11 @@ def main(args):
 		pass
 
 	if str(os.name) == "posix":
-		print("MacOS")
 		os.system("./seal seal.dat " + directory + " " + arguments)
 	if str(os.name) == 'nt':
-		print('Window')
 		os.system("seal.exe seal.dat " + directory + " " + arguments)
 
 if __name__ == "__main__":		
-	SEALROOT = str(pathlib.Path('data'))
-	print(SEALROOT)
-	main(sys.argv)
-	#main(SEALROOT)
+	SEALROOT = str(pathlib.Path(sys.argv[1]))
+	print("Chipping face from folder: " + SEALROOT)
+	main(SEALROOT)
