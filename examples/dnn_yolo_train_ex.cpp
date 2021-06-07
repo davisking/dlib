dccbaca627a4126bcb394dcade97127bd9ea187e
Adrià Arrufat <adria.arrufat@outlook.com>
2021-06-07 11:17:40 +0900

    add option to visualize the detections of the last training synchronization file

--- examples/dnn_yolo_train_ex.cpp
+++ examples/dnn_yolo_train_ex.cpp
@@ -1,7 +1,7 @@
 #include <dlib/cmd_line_parser.h>
 #include <dlib/data_io.h>
-#include <dlib/dir_nav.h>
 #include <dlib/dnn.h>
+#include <dlib/gui_widgets.h>
 #include <dlib/image_io.h>
 
 using namespace std;
@@ -27,7 +27,6 @@ namespace darknet
     template <typename SUBNET> using htag32 = add_tag_layer<7032, SUBNET>;
     template <typename SUBNET> using hskip8 = add_skip_layer<htag8, SUBNET>;
     template <typename SUBNET> using hskip16 = add_skip_layer<htag16, SUBNET>;
-    template <typename SUBNET> using hskip32 = add_skip_layer<htag32, SUBNET>;
     // yolo tags
     template <typename SUBNET> using ytag8 = add_tag_layer<4008, SUBNET>;
     template <typename SUBNET> using ytag16 = add_tag_layer<4016, SUBNET>;
@@ -72,7 +71,6 @@ namespace darknet
                                                  conblock<32, 3, 1,
                                                  INPUT>>>>>>>>>>>>>;
 
-
         template <long nf, int classes, template <typename> class YTAG, template <typename> class NTAG, typename SUBNET>
         using yolo = YTAG<sig<con<3 * (classes + 5), 1, 1, 1, 1,
                      conblock<nf, 3, 1,
@@ -98,23 +96,33 @@ namespace darknet
     using yolov3_infer = def<leaky_relu, affine>::yolov3<80>;
 
     template <typename net_type>
-    void setup_detector(net_type& net, int num_classes = 80, size_t img_size = 416)
+    void setup_detector(net_type& net, const yolo_options& options, long image_size)
     {
         // remove bias
         disable_duplicative_biases(net);
-        // remove mean from input image
-        layer<net_type::num_layers - 1>(net) = input_rgb_image(0, 0, 0);
+        // do not remove the mean from the input image
+        input_layer(net) = input_rgb_image(0, 0, 0);
         // setup leaky relus
         visit_computational_layers(net, [](leaky_relu_& l) { l = leaky_relu_(0.1); });
-        // set the number of filters
-        layer<ytag8, 2>(net).layer_details().set_num_filters(3 * (num_classes + 5));
-        layer<ytag16, 2>(net).layer_details().set_num_filters(3 * (num_classes + 5));
-        layer<ytag32, 2>(net).layer_details().set_num_filters(3 * (num_classes + 5));
+        // set the number of filters (they are located after the tag and sig layers)
+        layer<ytag8, 2>(net).layer_details().set_num_filters(3 * (options.labels.size() + 5));
+        layer<ytag16, 2>(net).layer_details().set_num_filters(3 * (options.labels.size() + 5));
+        layer<ytag32, 2>(net).layer_details().set_num_filters(3 * (options.labels.size() + 5));
         // allocate the network
-        matrix<rgb_pixel> image(img_size, img_size);
+        matrix<rgb_pixel> image(image_size, image_size);
         net(image);
     }
+}
 
+rectangle_transform preprocess_image(const matrix<rgb_pixel>& image, matrix<rgb_pixel>& output, const long image_size)
+{
+    return rectangle_transform(inv(letterbox_image(image, output, image_size)));
+}
+
+void postprocess_detections(const rectangle_transform& tform, std::vector<yolo_rect>& detections)
+{
+    for (auto& d : detections)
+        d.rect = tform(d.rect);
 }
 
 using net_type = dlib::loss_yolo<darknet::ytag8, darknet::ytag16, darknet::ytag32, darknet::yolov3_train>;
@@ -130,6 +138,7 @@ try
     parser.add_option("steps", "number of training steps (defaule: 500000)", 1);
     parser.add_option("workers", "number of worker threads to load data (default: 4)", 1);
     parser.add_option("gpus", "number of GPUs to run the training on (default: 1)", 1);
+    parser.add_option("test", "test the detector with a custom threshold (default: 0.01)", 1);
     parser.set_group_name("Help Options");
     parser.add_option("h", "alias of --help");
     parser.add_option("help", "display this message and exit");
@@ -147,11 +156,14 @@ try
     const size_t image_size = get_option(parser, "size", 416);
     const size_t num_workers = get_option(parser, "workers", 4);
     const size_t num_gpus = get_option(parser, "gpus", 1);
-    const std::string data_directory = parser[0];
+
+    const string data_directory = parser[0];
+    const string sync_file_name = "yolov3_sync";
+
     image_dataset_metadata::dataset dataset;
     image_dataset_metadata::load_image_dataset_metadata(dataset, data_directory + "/training.xml");
     std::cout << "# images: " << dataset.images.size() << std::endl;
-    std::map<std::string, size_t> labels;
+    std::map<string, size_t> labels;
     size_t num_objects = 0;
     for (const auto& im : dataset.images)
     {
@@ -161,22 +173,23 @@ try
             ++num_objects;
         }
     }
-    std::cout << "# labels: " << labels.size() << std::endl;
+    cout << "# labels: " << labels.size() << endl;
 
     yolo_options options;
     for (const auto& label : labels)
     {
-        std::cout <<  " - " << label.first << ": " << label.second;
-        std::cout << " (" << (100.0*label.second)/num_objects << "%)\n";
+        cout <<  " - " << label.first << ": " << label.second;
+        cout << " (" << (100.0*label.second)/num_objects << "%)\n";
         options.labels.push_back(label.first);
     }
     options.confidence_threshold = 0.25;
+    options.ignore_iou_threshold = 0.5;
     options.add_anchors<darknet::ytag8>({{10, 13}, {16, 30}, {33, 23}});
     options.add_anchors<darknet::ytag16>({{30, 61}, {62, 45}, {59, 119}});
     options.add_anchors<darknet::ytag32>({{116, 90}, {156, 198}, {373, 326}});
-    options.overlaps_nms = dlib::test_box_overlap(0.45);
+    options.overlaps_nms = test_box_overlap(0.45);
     net_type net(options);
-    darknet::setup_detector(net, options.labels.size(), image_size);
+    darknet::setup_detector(net, options, image_size);
 
     // Cosine scheduler with warm-up:
     // - learning_rate is the highest learning rate value, e.g. 0.01
@@ -185,18 +198,56 @@ try
     const matrix<double> learning_rate_schedule = learning_rate * join_rows(
         linspace(0, 1, warmup),
         ((1 + cos(pi / (num_steps - warmup) * linspace(0, num_steps - warmup, num_steps - warmup))) / 2)
-    ) + std::numeric_limits<double>::epsilon();  // this prevents the learning rates from being 0
+    ) + numeric_limits<double>::epsilon();  // this prevents the learning rates from being 0
 
     std::vector<int> gpus(num_gpus);
-    std::iota(gpus.begin(), gpus.end(), 0);
+    iota(gpus.begin(), gpus.end(), 0);
     dnn_trainer<net_type> trainer(net, sgd(0.0005, 0.9), gpus);
     trainer.be_verbose();
     trainer.set_mini_batch_size(batch_size);
     trainer.set_learning_rate_schedule(learning_rate_schedule);
-    trainer.set_synchronization_file("yolov3_sync", std::chrono::minutes(15));
-    std::cout << trainer;
-    std::cout << "  burnin: " << warmup << std::endl;
-    std::cout << "  #steps: " << num_steps << std::endl;
+    trainer.set_synchronization_file(sync_file_name, chrono::minutes(15));
+    cout << trainer;
+    cout << "  burnin: " << warmup << endl;
+    cout << "  #steps: " << num_steps << endl;
+
+    if (parser.option("test"))
+    {
+        if (!file_exists(sync_file_name))
+        {
+            cout << "Could not find file " << sync_file_name << endl;
+            return EXIT_FAILURE;
+        }
+        const double threshold = get_option(parser, "test", 0.01);
+        {
+            net_type temp;
+            dnn_trainer<net_type> trainer(temp);
+            trainer.set_synchronization_file(sync_file_name);
+            trainer.get_net();
+            net.subnet() = temp.subnet();
+        }
+        net.loss_details().adjust_threshold(threshold);
+        image_window win;
+        matrix<rgb_pixel> image, resized;
+        for (const auto& im : dataset.images)
+        {
+            win.clear_overlay();
+            load_image(image, data_directory + "/" + im.filename);
+            win.set_title(im.filename);
+            win.set_image(image);
+            const auto tform = preprocess_image(image, resized, image_size);
+            auto detections = net(resized);
+            postprocess_detections(tform, detections);
+            cout << "# detections: " << detections.size() << endl;
+            for (const auto& det : detections)
+            {
+                win.add_overlay(det.rect, rgb_pixel(255, 0, 0), det.label);
+                cout << det.label << ": " << det.rect << " " << det.detection_confidence << endl;
+            }
+            cin.get();
+        }
+        return EXIT_SUCCESS;
+    }
 
     dlib::pipe<std::pair<matrix<rgb_pixel>, std::vector<yolo_rect>>> train_data(1000);
     auto loader = [&dataset, &data_directory, &train_data, &image_size](time_t seed) {
@@ -222,7 +273,7 @@ try
         }
     };
 
-    std::vector<std::thread> data_loaders;
+    std::vector<thread> data_loaders;
     for (size_t i = 0; i < num_workers; ++i)
         data_loaders.emplace_back([loader, i]() { loader(i + 1); });
 
@@ -232,12 +283,12 @@ try
     {
         images.clear();
         bboxes.clear();
-        std::pair<matrix<rgb_pixel>, std::vector<yolo_rect>> temp;
+        pair<matrix<rgb_pixel>, std::vector<yolo_rect>> temp;
         while (images.size() < trainer.get_mini_batch_size())
         {
             train_data.dequeue(temp);
-            images.push_back(std::move(temp.first));
-            bboxes.push_back(std::move(temp.second));
+            images.push_back(move(temp.first));
+            bboxes.push_back(move(temp.second));
         }
         trainer.train_one_step(images, bboxes);
     }
@@ -251,6 +302,6 @@ try
 }
 catch (const std::exception& e)
 {
-    std::cout << e.what() << '\n';
+    cout << e.what() << endl;
     return EXIT_FAILURE;
 }
