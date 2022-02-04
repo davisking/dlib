c9cdd7b5037e4ee27ca58071591123cd5bb93dff
Juha Reunanen <juha.reunanen@tomaattinen.com>
2022-02-04 15:12:35 +0200

    Fix checking if we're using Visual C++

--- dlib/geometry/rectangle.h
+++ dlib/geometry/rectangle.h
@@ -750,7 +750,7 @@ namespace dlib
 
     // Circumvent what appears to be a bug in Visual Studio 2019's optimizer
     // (see: https://forum.juce.com/t/warning-in-the-lastest-vs2019/38267)
-#if defined (_MSVC_VER)
+#if defined (_MSC_VER)
 #pragma warning ( push )
 #pragma warning ( disable: 4723 )
 #endif
@@ -774,7 +774,7 @@ namespace dlib
             return centered_rect(rect, (long)std::round(rect.width()*scale), (long)std::round(rect.height()*scale));
         }
     }
-#if defined (_MSVC_VER)
+#if defined (_MSC_VER)
 #pragma warning ( pop )
 #endif
 
