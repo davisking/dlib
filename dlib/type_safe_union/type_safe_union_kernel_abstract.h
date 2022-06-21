7c046c922fd725868f70ec3267918480df72d6ab
pfeatherstone <peter@me>
2022-06-21 10:34:50 +0100

    added docs for swap(a,b) back in

--- dlib/type_safe_union/type_safe_union_kernel_abstract.h
+++ dlib/type_safe_union/type_safe_union_kernel_abstract.h
@@ -364,6 +364,17 @@ namespace dlib
 
 // ----------------------------------------------------------------------------------------
 
+    template<typename... Types>
+    inline void swap (
+        type_safe_union<Types...>& a,
+        type_safe_union<Types...>& b
+    ) { a.swap(b); }
+    /*!
+        provides a global swap function
+    !*/
+
+// ----------------------------------------------------------------------------------------
+
     template<
         typename TSU,
         typename F
