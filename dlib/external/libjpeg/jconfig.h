bb6efb1f97544bedb88853d361d6221ee1544deb
Adrià Arrufat <swiftscythe@gmail.com>
2022-07-04 22:09:19 +0900

    Comment out #define __inline__, which breaks Visual Studio

--- dlib/external/libjpeg/jconfig.h
+++ dlib/external/libjpeg/jconfig.h
@@ -35,7 +35,7 @@ typedef unsigned char boolean;
 #ifdef JPEG_INTERNALS
 
 /* #undef RIGHT_SHIFT_IS_UNSIGNED */
-#define INLINE __inline__
+/* #define INLINE __inline__ */
 /* These are for configuring the JPEG memory manager. */
 /* #undef DEFAULT_MAX_MEM */
 /* #undef NO_MKTEMP */
