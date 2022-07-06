f94ad31c80fc7399943b805d19432dc39d9aae6a
pfeatherstone <peter@me>
2022-07-06 13:02:13 +0100

    placeholder code

--- dlib/math/bessel.h
+++ dlib/math/bessel.h
@@ -3,12 +3,28 @@
 #ifndef DLIB_MATH_BESSEL
 #define DLIB_MATH_BESSEL
 
+#ifndef __cpp_lib_math_special_functions
+#include "details/bessel.h"
+#endif
+
 namespace dlib
 {
 #if __cpp_lib_math_special_functions
     using std::cyl_bessel_i;
     using std::cyl_bessel_j;
 #else
+
+    template<typename R>
+    R cyl_bessel_i(R nu, R x)
+    {
+        return detail::cyl_bessel_i(nu, x);
+    }
+
+    template<typename R>
+    R cyl_bessel_j(R nu, R x)
+    {
+        return detail::cyl_bessel_j(nu, x);
+    }
 #endif
 }
 
---
+++ dlib/math/details/bessel.h
@@ -0,0 +1,25 @@
+// Copyright (C) 2006  Davis E. King (davis@dlib.net)
+// License: Boost Software License   See LICENSE.txt for the full license.
+#ifndef DLIB_MATH_DETAIL_BESSEL
+#define DLIB_MATH_DETAIL_BESSEL
+
+namespace dlib
+{
+    namespace detail
+    {
+        template<typename R>
+        R cyl_bessel_i(R nu, R x)
+        {
+            //TODO
+            return R{};
+        }
+
+        template<typename R>
+        R cyl_bessel_j(R nu, R x)
+        {
+            //TODO
+            return R{};
+        }
+    }
+}
+#endif //DLIB_MATH_DETAIL_BESSEL
