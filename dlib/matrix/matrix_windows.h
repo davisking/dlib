4682c5d383b5609e3fd83b1f5be415d3bf9e86a2
pf <pf@me>
2022-07-14 22:13:47 +0100

    - added kaiser overloads for dlib::matrix

--- dlib/matrix/matrix_windows.h
+++ dlib/matrix/matrix_windows.h
@@ -61,6 +61,54 @@ namespace dlib
     DLIB_DEFINE_WINDOW_OP(blackman_nuttall)
     DLIB_DEFINE_WINDOW_OP(blackman_harris)
     DLIB_DEFINE_WINDOW_OP(blackman_harris7)
+
+// ----------------------------------------------------------------------------------------
+
+    template <typename M>
+    struct op_kaiser : basic_op_m<M>
+    {
+        typedef typename M::type type;
+
+        op_kaiser(const M& m_, beta_t beta_, bool symmetric) : basic_op_m<M>(m_), beta{beta_}, is_symmetric{symmetric}
+        {DLIB_ASSERT(is_vector(m_), "matrix expression must be a vector");}
+
+        beta_t beta;
+        bool is_symmetric = true;
+
+        const static long cost = M::cost + 7;
+        typedef type const_ret_type;
+        const_ret_type apply(long r, long c) const
+        {
+            const type win = is_symmetric ?
+                   kaiser<type>(index_t{r*this->m.nc()+c}, window_length{this->m.size()}, beta, symmetric_t{}) :
+                   kaiser<type>(index_t{r*this->m.nc()+c}, window_length{this->m.size()}, beta, periodic_t{})  ;
+            return win * this->m(r,c);
+        }
+    };
+
+    template <typename EXP>
+    const matrix_op<op_kaiser<EXP> > kaiser (
+        const matrix_exp<EXP>& m,
+        beta_t beta,
+        symmetric_t
+    )
+    {
+        using op = op_kaiser<EXP>;
+        return matrix_op<op>(op(m.ref(), beta, true));
+    }
+
+    template <typename EXP>
+    const matrix_op<op_kaiser<EXP> > kaiser (
+        const matrix_exp<EXP>& m,
+        beta_t beta,
+        periodic_t
+    )
+    {
+        using op = op_kaiser<EXP>;
+        return matrix_op<op>(op(m.ref(), beta, false));
+    }
+
+// ----------------------------------------------------------------------------------------
 }
 
 #endif //DLIB_MATRIX_WINDOWS_H
