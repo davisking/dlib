190a614f9486226a513903c91b5c4dda481d23d0
pf <pf@me>
2023-01-14 11:29:53 +0000

    hopefully more educational

--- examples/ffmpeg_video_demuxing.cpp
+++ examples/ffmpeg_video_demuxing.cpp
@@ -20,10 +20,10 @@ int main(const int argc, const char** argv)
 try
 {
     command_line_parser parser;
-    parser.add_option("i", "input video", 1);
+    parser.add_option("i",      "input video", 1);
     parser.set_group_name("Help Options");
-    parser.add_option("h", "alias of --help");
-    parser.add_option("help", "display this message and exit");
+    parser.add_option("h",      "alias of --help");
+    parser.add_option("help",   "display this message and exit");
 
     parser.parse(argc, argv);
     const char* one_time_opts[] = {"i"};
@@ -39,11 +39,55 @@ try
 
     image_window win;
 
-    // For simplicity we use the constructor which takes a filepath only. All other parameters are defaulted or guessed.
-    // We could have instead done this:
-    //      ffmpeg::demuxer::args args;
-    //      args.filepath = filepath;
-    //      ffmpeg::demuxer cap(args);
+    /*
+        For simplicity we use the constructor which takes a filepath only. All other parameters are defaulted or guessed.
+        Equivalently, we could have done:
+
+            ffmpeg::demuxer::args args;
+            args.filepath = filepath;
+            ffmpeg::demuxer cap(args);
+
+        Furthermore, we can set additional settings in args, for example:
+
+            // This disables extracting and decoding images. 
+            // You may want to do this if you're only interesting in extracting audio
+            args.enable_image = false; 
+
+            // This disables extracting and decoding audio.
+            // You may want to do this if you don't care about audio in your video. 
+            // This saves processing time and you don't have to deal with audio frame objects in your code.
+            args.enable_audio = false;
+
+            // This will resize frames before presenting them to the user. 
+            // I.e. frames "returned" by demuxer::read() will have this height.
+            // By default, the demuxer object does not resize frames.
+            args.image_options.h = SOME_HEIGHT;
+
+            // Same as above but for width.
+            args.image_options.w = SOME_WIDTH;
+
+            // By default, demuxer reformats frames from the default format in the encoded stream to RGB.
+            // You can set this to AV_PIX_FMT_NONE and demuxer will leave frames in their default format.
+            // This is likely to be AV_PIX_FMT_YUV420P.
+            // However, you can set it to anything that FFMPEG supports, and frames will be presented
+            // in that format.
+            args.image_options.fmt = SOME_OTHER_PIXEL_FORMAT;
+
+            // Same as above, by default, demuxer leaves audio frames in their default sample rate.
+            // But user can change this, and audio will be resampled to that rate.
+            // Note, reducing the sample rate reduces the quality of the audio.
+            // You can artificially upsample audio, but it won't make the quality any better.
+            args.audio_options.sample_rate = SOME_SAMPLE_RATE;
+
+            // You may want to do this if you want more or less channels.
+            // Note, dlib only has one audio object "audio_frame", which is stereo and uses int16_t sample format.
+            // So if you're going to use other layouts and sample formats, you won't be able to use audio_frame.
+            // You will have to use ffmpeg::frame directly. Use with care and please visit ffmpeg's documentation.
+            args.audio_options.channel_layout = SOME_OTHER_LAYOUT; // e.g. AV_CH_LAYOUT_MONO, AV_CH_LAYOUT_STEREO. See libavutil/channel_layout.h
+
+            // This changes the default sample format.
+            args.audio_options.fmt = SOME_OTHER_SAMPLE_FORMAT;
+    */
 
     ffmpeg::demuxer cap(filepath);
     if (!cap.is_open())
@@ -64,7 +108,7 @@ try
         printf("    nframes             : %d\n", cap.estimated_nframes());
         printf("    codec               : %s\n", cap.get_video_codec_name().c_str());
     }
-    printf("Video contains audio : %i\n", cap.audio_enabled());
+    printf("Video contains audio    : %i\n", cap.audio_enabled());
     if (cap.audio_enabled())
     {
         printf("    sample rate         : %i\n", cap.sample_rate());
@@ -76,17 +120,14 @@ try
     }
 
     printf("\n\n");
-    printf("Video metadata:\n\n");
+    printf("Video metadata:\n");
    
-    for (auto&& stream_metadata_pair : cap.get_all_metadata())
-    {
-        printf("    Stream %i\n", stream_metadata_pair.first);
-        for (auto&& metadata : stream_metadata_pair.second)
-            printf("        key `%s` : val `%s`\n", metadata.first.c_str(), metadata.second.c_str());
-    }
+    for (auto&& metadata : cap.get_metadata())
+        printf("    key : %-32s ; val : %-32s\n", metadata.first.c_str(), metadata.second.c_str());
 
     ffmpeg::frame frame;
     type_safe_union<array2d<rgb_pixel>, ffmpeg::audio_frame> obj;
+    size_t audio_samples{0};
 
     while (cap.read(frame))
     {
@@ -97,28 +138,35 @@ try
             {
                 win.set_image(img);
             },
-            [](const ffmpeg::audio_frame& /*audio*/)
+            [&](const ffmpeg::audio_frame& audio)
             {
-                //You could play through your speaker here if you wanted
+                audio_samples += audio.samples.size();
+                printf("\r\tDecoding %zu samples", audio_samples); fflush(stdout);
             }
         ), obj);
 
-        // Alternatively, you could have done:
-        // array2d<rgb_pixel>  img;
-        // ffmpeg::audio_frame audio;
+        /*
+            Alternatively, you could have done:
 
-        // if (frame.is_image() && frame.pixfmt() == AV_PIX_FMT_RGB24)
-        // {
-        //     convert(frame, img);
-        //     win.set_image(img);
-        // }
+                array2d<rgb_pixel>  img;
+                ffmpeg::audio_frame audio;
 
-        // if (frame.is_audio() && frame.samplefmt() == AV_SAMPLE_FMT_S16)
-        // {
-        //     convert(frame, audio);
-        //     // Do something useful, maybe play through your speaker? 
-        // }
+                if (frame.is_image() && frame.pixfmt() == AV_PIX_FMT_RGB24)
+                {
+                    convert(frame, img);
+                    win.set_image(img);
+                }
+
+                if (frame.is_audio() && frame.samplefmt() == AV_SAMPLE_FMT_S16)
+                {
+                    convert(frame, audio);
+                    // Do something useful, maybe play through your speaker? 
+                }
+
+        */
     }
+
+    printf("\n");
 
     return EXIT_SUCCESS;
 }
