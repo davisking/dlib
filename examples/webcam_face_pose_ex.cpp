e1f003c5240e1f0cbab0796b2981a1181c0bdc91
pf <pf@me>
2023-01-25 09:11:32 +0000

    revert

--- examples/webcam_face_pose_ex.cpp
+++ examples/webcam_face_pose_ex.cpp
@@ -1,13 +1,17 @@
 // The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
 /*
+
     This example program shows how to find frontal human faces in an image and
     estimate their pose.  The pose takes the form of 68 landmarks.  These are
     points on the face such as the corners of the mouth, along the eyebrows, on
     the eyes, and so forth.  
     
+
     This example is essentially just a version of the face_landmark_detection_ex.cpp
     example modified to use OpenCV's VideoCapture object to read from a camera instead 
     of files.
+
+
     Finally, note that the face detector is fastest when compiled with at least
     SSE2 instructions enabled.  So if you are using a PC with an Intel or AMD
     chip then you should enable at least SSE2 instructions.  If you are using
