58f990d14188f2688e1b7c097795f7649da3b8e9
pfeatherstone <45853521+pfeatherstone@users.noreply.github.com>
2023-02-20 12:53:59 +0000

    Update ffmpeg_utils.h
    typo

--- dlib/media/ffmpeg_utils.h
+++ dlib/media/ffmpeg_utils.h
@@ -805,7 +805,7 @@ namespace dlib
             // The ffmpeg documentation recommends you always use align==0.
             // However, in ffmpeg 3.2, there is a bug where if you do that, data buffers don't get allocated.
             // So workaround is to manually set align==32
-            // Not ideal, but i've checked the source code in ffmpeg 3.3, 4.4 and 5.0 libavutil.c
+            // Not ideal, but i've checked the source code in ffmpeg 3.3, 4.4 and 5.0 libavutil/frame.c
             // and in every case, if align==0, then align=32
             const int align = 32;
 
