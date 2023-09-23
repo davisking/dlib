d0a565fe814b029ef24dc43dfeceb1bf2e3272ed
Your Name <you@example.com>
2023-09-24 00:28:27 +0100

    bug fix

--- dlib/type_traits.h
+++ dlib/type_traits.h
@@ -596,7 +596,7 @@ namespace dlib
     template<template<class...> class Primary, class... Args>
     struct is_specialization_of<Primary<Args...>, Primary> : std::true_type{};
 
-#ifdef __cpp_variable_templates
+#ifdef __cpp_inline_variables
     template<class T, template<class...> class Primary>
     inline constexpr bool is_specialization_of_v = is_specialization_of<T, Primary>::value;
 #endif
