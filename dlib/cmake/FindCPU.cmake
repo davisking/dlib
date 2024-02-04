2e294d4d45dfa24e967827086ab97685efd26f50
me <me@me.com>
2024-02-04 08:42:33 +0000

    find neon

--- dlib/cmake/FindCPU.cmake
+++ dlib/cmake/FindCPU.cmake
@@ -84,4 +84,73 @@ if (HAVE_SSE42)
     set(SSE42_CFLAGS ${CMAKE_REQUIRED_FLAGS})
 endif()
 
-find_package_handle_standard_args(AVX DEFAULT_MSG HAVE_AVX)
+# Test for SSE 4.1
+if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    set(CMAKE_REQUIRED_FLAGS "-msse4.1")
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+    set(CMAKE_REQUIRED_FLAGS "-xHost")
+elseif(MSVC AND SIZE_OF_VOID_PTR EQUAL 4)
+    set(CMAKE_REQUIRED_FLAGS "/arch:SSE2")
+endif()
+
+check_c_source_runs(
+"
+#include <emmintrin.h>
+#include <smmintrin.h>
+int main()
+{
+    long long a[2] = {  1, 2 };
+    long long b[2] = { -1, 2 };
+    long long c[2];
+    __m128i va = _mm_loadu_si128((__m128i*)a);
+    __m128i vb = _mm_loadu_si128((__m128i*)b);
+    __m128i vc = _mm_cmpeq_epi64(va, vb);
+
+    _mm_storeu_si128((__m128i*)c, vc);
+    if (c[0] == 0LL && c[1] == -1LL)
+      return 0;
+    else
+      return 1;
+}" HAVE_SSE41)
+
+if (HAVE_SSE41)
+    set(SSE41_CFLAGS ${CMAKE_REQUIRED_FLAGS})
+endif()
+
+# Test for NEON
+if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    set(CMAKE_REQUIRED_FLAGS "-mfpu=neon")
+else()
+    set(CMAKE_REQUIRED_FLAGS "")
+endif()
+
+check_c_source_runs(
+"
+#include <stdio.h>
+#include <arm_neon.h>
+
+int main(void){
+//vector addition 8x8 example.
+int r,s;
+
+uint8x8_t a = vdup_n_u8(9);
+uint8x8_t b = vdup_n_u8(10);
+
+uint8x8_t dst = a * b;
+
+r = vget_lane_u8( dst, 0);
+s = vget_lane_u8( dst, 7);
+
+if (r != 90 || s != 90) {
+   return 1;
+}
+
+return 0;
+}" HAVE_NEON)
+
+if (HAVE_NEON)
+    set(NEON_CFLAGS ${CMAKE_REQUIRED_FLAGS})
+endif()
+
+
+find_package_handle_standard_args(CPU DEFAULT_MSG HAVE_AVX)
