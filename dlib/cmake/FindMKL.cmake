09e0fecbc4e49877e045f752e602433c03a6c337
Your Name <you@example.com>
2024-02-16 15:46:47 +0000

    - look for tbb. We might need that soon if the tbb symbols in libmkl_tbb_thread.so are weak - install MKL via official installer

--- dlib/cmake/FindMKL.cmake
+++ dlib/cmake/FindMKL.cmake
@@ -16,7 +16,8 @@ include(CheckTypeSize)
 check_type_size( "void*" SIZE_OF_VOID_PTR)
 
 set(mkl_search_path
-    /opt/intel/oneapi/mkl/latest)
+    /opt/intel/oneapi/mkl/latest
+    /opt/intel/oneapi/tbb/latest)
 
 if (SIZE_OF_VOID_PTR EQUAL 8)
     set(mkl_lib_suffix lib)
@@ -26,16 +27,18 @@ else()
     set(mkl_intel_name mkl_intel)
 endif()
 
-find_path(MKL_INCLUDE_DIR       NAMES mkl_version.h     PATHS ${mkl_search_path} PATH_SUFFIXES mkl include)
-find_library(MKL_CORE_LIB       NAMES mkl_core          PATHS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})
-find_library(MKL_SEQUENTIAL_LIB NAMES mkl_sequential    PATHS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})
-find_library(MKL_TBB_LIB        NAMES mkl_tbb_thread    PATHS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})
-find_library(MKL_INTEL_LIB      NAMES ${mkl_intel_name} PATHS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})  
+find_path(MKL_INCLUDE_DIR       NAMES mkl_version.h     HINTS ${mkl_search_path} PATH_SUFFIXES mkl include)
+find_library(MKL_CORE_LIB       NAMES mkl_core          HINTS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})
+find_library(MKL_SEQUENTIAL_LIB NAMES mkl_sequential    HINTS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})
+find_library(MKL_TBB_LIB        NAMES mkl_tbb_thread    HINTS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})
+find_library(MKL_INTEL_LIB      NAMES ${mkl_intel_name} HINTS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})  
+find_library(TBB_LIB            NAMES tbb               HINTS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})  
 
 mark_as_advanced(MKL_INCLUDE_DIR)
 mark_as_advanced(MKL_CORE_LIB)
 mark_as_advanced(MKL_INTEL_LIB)
 mark_as_advanced(MKL_SEQUENTIAL_LIB MKL_TBB_LIB)
+mark_as_advanced(TBB_LIB)
 
 add_library(mkl_core UNKNOWN IMPORTED)
 set_target_properties(mkl_core PROPERTIES 
@@ -57,10 +60,14 @@ set_target_properties(mkl_tbb PROPERTIES
     IMPORTED_LOCATION               ${MKL_TBB_LIB}
     INTERFACE_INCLUDE_DIRECTORIES   ${MKL_INCLUDE_DIR})
 
+add_library(tbb UNKNOWN IMPORTED)
+set_target_properties(tbb PROPERTIES 
+    IMPORTED_LOCATION ${TBB_LIB})
+
 add_library(mkl::sequential INTERFACE IMPORTED)
 target_link_libraries(mkl::sequential INTERFACE mkl_intel mkl_sequential mkl_core)
 
 add_library(mkl::tbb INTERFACE IMPORTED)
 target_link_libraries(mkl::tbb INTERFACE mkl_intel mkl_tbb mkl_core)
 
-find_package_handle_standard_args(MKL DEFAULT_MSG MKL_INCLUDE_DIR MKL_CORE_LIB MKL_INTEL_LIB MKL_SEQUENTIAL_LIB MKL_TBB_LIB)
+find_package_handle_standard_args(MKL DEFAULT_MSG MKL_CORE_LIB MKL_INTEL_LIB MKL_SEQUENTIAL_LIB MKL_TBB_LIB MKL_INCLUDE_DIR)
