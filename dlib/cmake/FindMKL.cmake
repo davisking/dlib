bfe8b49a2d9b332e9808740841efba56ffff7bd6
pfeatherstone <pfeatherstone@pf>
2024-02-17 21:27:33 +0000

    added GNU OMP threading layer. Made TBB layer the default.

--- dlib/cmake/FindMKL.cmake
+++ dlib/cmake/FindMKL.cmake
@@ -14,6 +14,7 @@
 include(FindPackageHandleStandardArgs)
 include(CheckTypeSize)
 check_type_size( "void*" SIZE_OF_VOID_PTR)
+find_package(OpenMP)
 
 set(mkl_search_path
     /opt/intel/oneapi/mkl/latest
@@ -31,13 +32,16 @@ find_path(MKL_INCLUDE_DIR       NAMES mkl_version.h     HINTS ${mkl_search_path}
 find_library(MKL_CORE_LIB       NAMES mkl_core          HINTS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})
 find_library(MKL_SEQUENTIAL_LIB NAMES mkl_sequential    HINTS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})
 find_library(MKL_TBB_LIB        NAMES mkl_tbb_thread    HINTS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})
+find_library(MKL_GNU_LIB        NAMES mkl_gnu_thread    HINTS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})
 find_library(MKL_INTEL_LIB      NAMES ${mkl_intel_name} HINTS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})  
 find_library(TBB_LIB            NAMES tbb               HINTS ${mkl_search_path} PATH_SUFFIXES ${mkl_lib_suffix})  
 
 mark_as_advanced(MKL_INCLUDE_DIR)
 mark_as_advanced(MKL_CORE_LIB)
 mark_as_advanced(MKL_INTEL_LIB)
-mark_as_advanced(MKL_SEQUENTIAL_LIB MKL_TBB_LIB)
+mark_as_advanced(MKL_SEQUENTIAL_LIB)
+mark_as_advanced(MKL_TBB_LIB)
+mark_as_advanced(MKL_GNU_LIB)
 mark_as_advanced(TBB_LIB)
 
 add_library(mkl_core UNKNOWN IMPORTED)
@@ -64,10 +68,18 @@ add_library(tbb UNKNOWN IMPORTED)
 set_target_properties(tbb PROPERTIES 
     IMPORTED_LOCATION ${TBB_LIB})
 
+add_library(mkl_gnu UNKNOWN IMPORTED)
+set_target_properties(mkl_gnu PROPERTIES 
+    IMPORTED_LOCATION               ${MKL_GNU_LIB}
+    INTERFACE_INCLUDE_DIRECTORIES   ${MKL_INCLUDE_DIR})
+
 add_library(mkl::sequential INTERFACE IMPORTED)
 target_link_libraries(mkl::sequential INTERFACE mkl_intel mkl_sequential mkl_core)
 
 add_library(mkl::tbb INTERFACE IMPORTED)
 target_link_libraries(mkl::tbb INTERFACE mkl_intel mkl_tbb mkl_core tbb)
 
-find_package_handle_standard_args(MKL DEFAULT_MSG MKL_CORE_LIB MKL_INTEL_LIB MKL_SEQUENTIAL_LIB MKL_TBB_LIB MKL_INCLUDE_DIR TBB_LIB)
+add_library(mkl::gnu INTERFACE IMPORTED)
+target_link_libraries(mkl::gnu INTERFACE mkl_intel mkl_gnu mkl_core OpenMP::OpenMP_CXX)
+
+find_package_handle_standard_args(MKL DEFAULT_MSG MKL_CORE_LIB MKL_INTEL_LIB MKL_SEQUENTIAL_LIB MKL_TBB_LIB MKL_INCLUDE_DIR TBB_LIB MKL_GNU_LIB)
