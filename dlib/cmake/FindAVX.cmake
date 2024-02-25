92e11fc70e89c5a50ea251253d047c1a8ee0ffe4
pfeatherstone <pfeatherstone@pf>
2024-02-25 08:55:56 +0000

    - change the order of the SIMD compiler options. e.g. it's -mavx -mavx2 not -mavx2 -mavx - don't need set set the compile definitions explicitly. simd_check.h does it for you - modified the stats stuff ever so slightly. It's basically just style. But that seems to make auto-vectorization stuff a bit happier

--- dlib/cmake/FindAVX.cmake
+++ dlib/cmake/FindAVX.cmake
@@ -3,30 +3,6 @@ include(CheckCSourceRuns)
 include(CheckTypeSize)
 check_type_size( "void*" SIZE_OF_VOID_PTR)
 
-# Test for AVX2
-if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    set(CMAKE_REQUIRED_FLAGS "-mavx2")
-elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
-    set(CMAKE_REQUIRED_FLAGS "-xHost")
-elseif(MSVC AND SIZE_OF_VOID_PTR EQUAL 4)
-    set(CMAKE_REQUIRED_FLAGS "/arch:AVX2")
-endif()
-
-check_c_source_runs(
-"
-#include <immintrin.h>
-int main()
-{
-    __m256i a       = _mm256_set_epi32 (-1, 2, -3, 4, -1, 2, -3, 4);
-    __m256i result  = _mm256_abs_epi32 (a);
-    return 0;
-}
-" HAVE_AVX2)
-
-if (HAVE_AVX2)
-    list(APPEND AVX_CFLAGS ${CMAKE_REQUIRED_FLAGS})
-endif()
-
 # Test for AVX
 if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     set(CMAKE_REQUIRED_FLAGS "-mavx")
@@ -48,6 +24,30 @@ int main()
 }" HAVE_AVX)
 
 if (HAVE_AVX)
+    list(APPEND AVX_CFLAGS ${CMAKE_REQUIRED_FLAGS})
+endif()
+
+# Test for AVX2
+if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    set(CMAKE_REQUIRED_FLAGS "-mavx2")
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+    set(CMAKE_REQUIRED_FLAGS "-xHost")
+elseif(MSVC AND SIZE_OF_VOID_PTR EQUAL 4)
+    set(CMAKE_REQUIRED_FLAGS "/arch:AVX2")
+endif()
+
+check_c_source_runs(
+"
+#include <immintrin.h>
+int main()
+{
+    __m256i a       = _mm256_set_epi32 (-1, 2, -3, 4, -1, 2, -3, 4);
+    __m256i result  = _mm256_abs_epi32 (a);
+    return 0;
+}
+" HAVE_AVX2)
+
+if (HAVE_AVX2)
     list(APPEND AVX_CFLAGS ${CMAKE_REQUIRED_FLAGS})
 endif()
 
--- dlib/cmake/FindSSE.cmake
+++ dlib/cmake/FindSSE.cmake
@@ -3,9 +3,9 @@ include(CheckCSourceRuns)
 include(CheckTypeSize)
 check_type_size( "void*" SIZE_OF_VOID_PTR)
 
-# Test for SSE 4.2
+# Test for SSE 2
 if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    set(CMAKE_REQUIRED_FLAGS "-msse4.2")
+    set(CMAKE_REQUIRED_FLAGS "-msse2")
 elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
     set(CMAKE_REQUIRED_FLAGS "-xHost")
 elseif(MSVC AND SIZE_OF_VOID_PTR EQUAL 4)
@@ -15,57 +15,25 @@ endif()
 check_c_source_runs(
 "
 #include <emmintrin.h>
-#include <nmmintrin.h>
-int main()
-{
-    long long a[2] = {  1, 2 };
-    long long b[2] = { -1, 3 };
-    long long c[2];
+  int main()
+  {
+    int a[4] = { 1, 2,  3,  4 };
+    int b[4] = { 3, 6, -4, -4 };
+    int c[4];
+
     __m128i va = _mm_loadu_si128((__m128i*)a);
     __m128i vb = _mm_loadu_si128((__m128i*)b);
-    __m128i vc = _mm_cmpgt_epi64(va, vb);
+    __m128i vc = _mm_add_epi32(va, vb);
 
     _mm_storeu_si128((__m128i*)c, vc);
-    if (c[0] == -1LL && c[1] == 0LL)
+    if (c[0] == 4 && c[1] == 8 && c[2] == -1 && c[3] == 0)
       return 0;
     else
       return 1;
-}" HAVE_SSE42)
+  }
+" HAVE_SSE2)
 
-if (HAVE_SSE42)
-    list(APPEND SSE_CFLAGS ${CMAKE_REQUIRED_FLAGS})
-endif()
-
-# Test for SSE 4.1
-if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    set(CMAKE_REQUIRED_FLAGS "-msse4.1")
-elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
-    set(CMAKE_REQUIRED_FLAGS "-xHost")
-elseif(MSVC AND SIZE_OF_VOID_PTR EQUAL 4)
-    set(CMAKE_REQUIRED_FLAGS "/arch:SSE2")
-endif()
-
-check_c_source_runs(
-"
-#include <emmintrin.h>
-#include <smmintrin.h>
-int main()
-{
-    long long a[2] = {  1, 2 };
-    long long b[2] = { -1, 2 };
-    long long c[2];
-    __m128i va = _mm_loadu_si128((__m128i*)a);
-    __m128i vb = _mm_loadu_si128((__m128i*)b);
-    __m128i vc = _mm_cmpeq_epi64(va, vb);
-
-    _mm_storeu_si128((__m128i*)c, vc);
-    if (c[0] == 0LL && c[1] == -1LL)
-      return 0;
-    else
-      return 1;
-}" HAVE_SSE41)
-
-if (HAVE_SSE41)
+if (HAVE_SSE2)
     list(APPEND SSE_CFLAGS ${CMAKE_REQUIRED_FLAGS})
 endif()
 
@@ -109,9 +77,9 @@ if (HAVE_SSE3)
     list(APPEND SSE_CFLAGS ${CMAKE_REQUIRED_FLAGS})
 endif()
 
-# Test for SSE 2
+# Test for SSE 4.1
 if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    set(CMAKE_REQUIRED_FLAGS "-msse2")
+    set(CMAKE_REQUIRED_FLAGS "-msse4.1")
 elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
     set(CMAKE_REQUIRED_FLAGS "-xHost")
 elseif(MSVC AND SIZE_OF_VOID_PTR EQUAL 4)
@@ -121,25 +89,57 @@ endif()
 check_c_source_runs(
 "
 #include <emmintrin.h>
-  int main()
-  {
-    int a[4] = { 1, 2,  3,  4 };
-    int b[4] = { 3, 6, -4, -4 };
-    int c[4];
-
+#include <smmintrin.h>
+int main()
+{
+    long long a[2] = {  1, 2 };
+    long long b[2] = { -1, 2 };
+    long long c[2];
     __m128i va = _mm_loadu_si128((__m128i*)a);
     __m128i vb = _mm_loadu_si128((__m128i*)b);
-    __m128i vc = _mm_add_epi32(va, vb);
+    __m128i vc = _mm_cmpeq_epi64(va, vb);
 
     _mm_storeu_si128((__m128i*)c, vc);
-    if (c[0] == 4 && c[1] == 8 && c[2] == -1 && c[3] == 0)
+    if (c[0] == 0LL && c[1] == -1LL)
       return 0;
     else
       return 1;
-  }
-" HAVE_SSE2)
+}" HAVE_SSE41)
 
-if (HAVE_SSE2)
+if (HAVE_SSE41)
+    list(APPEND SSE_CFLAGS ${CMAKE_REQUIRED_FLAGS})
+endif()
+
+# Test for SSE 4.2
+if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    set(CMAKE_REQUIRED_FLAGS "-msse4.2")
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+    set(CMAKE_REQUIRED_FLAGS "-xHost")
+elseif(MSVC AND SIZE_OF_VOID_PTR EQUAL 4)
+    set(CMAKE_REQUIRED_FLAGS "/arch:SSE2")
+endif()
+
+check_c_source_runs(
+"
+#include <emmintrin.h>
+#include <nmmintrin.h>
+int main()
+{
+    long long a[2] = {  1, 2 };
+    long long b[2] = { -1, 3 };
+    long long c[2];
+    __m128i va = _mm_loadu_si128((__m128i*)a);
+    __m128i vb = _mm_loadu_si128((__m128i*)b);
+    __m128i vc = _mm_cmpgt_epi64(va, vb);
+
+    _mm_storeu_si128((__m128i*)c, vc);
+    if (c[0] == -1LL && c[1] == 0LL)
+      return 0;
+    else
+      return 1;
+}" HAVE_SSE42)
+
+if (HAVE_SSE42)
     list(APPEND SSE_CFLAGS ${CMAKE_REQUIRED_FLAGS})
 endif()
 
