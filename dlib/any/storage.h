1c697881bac1809858cffb57f97065ae66a364c3
ribbon-otter <ribbon-otter@github.invalid>
2025-08-06 23:33:10 -0700

    std::aligned_storage_t is deprecated in c++23

--- dlib/any/storage.h
+++ dlib/any/storage.h
@@ -525,7 +525,7 @@ namespace dlib
             }
 
         private:
-            std::aligned_storage_t<Size, Alignment> data;
+            alignas(Alignment) unsigned char data[Size];
             void (*del)(storage_stack&)                         = nullptr;
             void (*copy)(const storage_stack&, storage_stack&)  = nullptr;
             void (*move)(storage_stack&, storage_stack&)        = nullptr;
@@ -773,7 +773,7 @@ namespace dlib
             }
 
         private:
-            std::aligned_storage_t<Size, Alignment> data;
+            alignas(Alignment) unsigned char data[Size];
             void* ptr                                       = nullptr;
             void (*del)(storage_sbo&)                       = nullptr;
             void (*copy)(const storage_sbo&, storage_sbo&)  = nullptr;
