56d1e451a706402ffddcc08b5f8558f1a1fcb942
Aldric PIERRAIN <aldric.pierrain@atos.net>
2025-10-30 23:39:11 +0100

    Update

--- examples/slm_advanced_train_ex.cpp
+++ examples/slm_advanced_train_ex.cpp
@@ -746,8 +746,8 @@ int main(int argc, char** argv)
             while (total_bytes < target_size && next_token != start_of_text && next_token != end_of_text
                 && !g_terminate_flag.load()) {
                 // Predict next token
-                auto out_token = net(input_seq);
-                next_token = static_cast<int>(out_token);
+                auto out_token = net(std::vector<matrix<int, 0, 1>>{ input_seq, input_seq });
+                next_token = static_cast<int>(out_token[0]);
                 token_buffer.push_back(next_token);
                 token_count++;
 
