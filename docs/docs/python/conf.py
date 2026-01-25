7433a9cfd6b6e9c6909f07e8d4b07598f3269d70
Davis King <davis@dlib.net>
2026-01-24 19:05:12 -0500

    update to use newer sphinx

--- docs/docs/python/conf.py
+++ docs/docs/python/conf.py
@@ -136,7 +136,9 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {
+    "**": []
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
